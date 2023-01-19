deba1295e9961fb9e477266eaa678fd232a6a5ba
shkisme <shkisme0130@gmail.com>
2023-01-19 09:55:11 +0900

    feat: init project

---
+++ src/main/java/com/keeper/homepage/HomepageApplication.java
@@ -0,0 +1,13 @@
+package com.keeper.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keeper/homepage/HomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keeper.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
