a076f6c57afab40ed6e63d67d7d0af8a606355c5
shkisme <shkisme0130@gmail.com>
2023-01-21 12:19:01 +0900

    feat: StaticWriteSubtitleImage Entity, Repository 생성

---
+++ src/main/java/com/keeper/homepage/domain/about/dao/StaticWriteSubtitleImageRepository.java
@@ -0,0 +1,9 @@
+package com.keeper.homepage.domain.about.dao;
+
+import com.keeper.homepage.domain.about.entity.StaticWriteSubtitleImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaticWriteSubtitleImageRepository extends
+    JpaRepository<StaticWriteSubtitleImage, Long> {
+
+}
---
+++ src/main/java/com/keeper/homepage/domain/about/entity/StaticWriteSubtitleImage.java
@@ -0,0 +1,43 @@
+package com.keeper.homepage.domain.about.entity;
+
+import static jakarta.persistence.CascadeType.REMOVE;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import com.keeper.homepage.domain.thumbnail.entity.Thumbnail;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "static_write_subtitle_image")
+public class StaticWriteSubtitleImage {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false)
+  private Long id;
+
+  @Column(name = "subtitle", nullable = false)
+  private String subtitle;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "static_write_title_id", nullable = false)
+  private StaticWriteTitle staticWriteTitle;
+
+  @ManyToOne(fetch = FetchType.LAZY, cascade = REMOVE)
+  @JoinColumn(name = "thumbnail_id")
+  private Thumbnail thumbnail;
+
+  @Column(name = "display_order", nullable = false)
+  private int displayOrder;
+}
