c085191df9c6eae89fd31d0e36e7c8e681f9ffb8
shkisme <shkisme0130@gmail.com>
2023-01-21 12:19:59 +0900

    feat: StaticWriteContent Entity, Repository 생성

---
+++ src/main/java/com/keeper/homepage/domain/about/dao/StaticWriteContentRepository.java
@@ -0,0 +1,8 @@
+package com.keeper.homepage.domain.about.dao;
+
+import com.keeper.homepage.domain.about.entity.StaticWriteContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaticWriteContentRepository extends JpaRepository<StaticWriteContent, Long> {
+
+}
