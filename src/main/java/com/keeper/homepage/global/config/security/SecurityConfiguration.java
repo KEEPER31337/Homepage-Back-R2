746e29e36b4f18b358c529bdd023c67b1732326e
정핸모 <gusah009@naver.com>
2023-01-22 18:03:33 +0900

    feat: web server에서 URL로 파일 바로 가져올 수 있게 수정

--- src/main/java/com/keeper/homepage/global/config/security/SecurityConfiguration.java
+++ src/main/java/com/keeper/homepage/global/config/security/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests()
-        .requestMatchers("/docs/*", "/auth-test").permitAll()
+        .requestMatchers("/docs/*", "/keeper_files/**", "/auth-test").permitAll()
         .anyRequest().hasRole("회원")
         .and()
         .httpBasic().disable()
--- src/main/java/com/keeper/homepage/global/config/web/WebMvcConfiguration.java
+++ src/main/java/com/keeper/homepage/global/config/web/WebMvcConfiguration.java
@@ -1,11 +1,16 @@
 package com.keeper.homepage.global.config.web;
 
+import static com.keeper.homepage.global.util.file.server.FileServerConstants.RESOURCE_PATH;
+import static com.keeper.homepage.global.util.file.server.FileServerConstants.ROOT_PATH;
+import static java.io.File.separator;
+
 import com.keeper.homepage.global.config.security.JwtTokenProvider;
 import com.keeper.homepage.global.config.security.annotation.AuthIdArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,5 +22,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
     resolvers.add(new AuthIdArgumentResolver(jwtTokenProvider));
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/keeper_files/**")
+        .addResourceLocations("file:" + RESOURCE_PATH)
+        .setCachePeriod(3600);
   }
 }
