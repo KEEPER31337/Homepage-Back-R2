8771e940e011c3449bed80fa2f1691d5483bd555
정핸모 <gusah009@naver.com>
2023-01-24 08:40:45 +0900

    refactor: IP 헤더 정보 enum으로 관리

--- src/main/java/com/keeper/homepage/global/util/web/WebUtil.java
+++ src/main/java/com/keeper/homepage/global/util/web/WebUtil.java
@@ -1,8 +1,11 @@
 package com.keeper.homepage.global.util.web;
 
 
+import jakarta.servlet.ServletRequest;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.function.Function;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -11,28 +14,35 @@ public class WebUtil {
   public static String getUserIP() {
     HttpServletRequest request = getHttpServletRequestByContext();
 
-    String ip = request.getHeader("X-FORWARDED-FOR");
-    if (ip == null) {
-      ip = request.getHeader("Proxy-Client-IP");
-    }
-    if (ip == null) {
-      ip = request.getHeader("WL-Proxy-Client-IP");
-    }
-    if (ip == null) {
-      ip = request.getHeader("HTTP_CLIENT_IP");
-    }
-    if (ip == null) {
-      ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-    }
-    if (ip == null) {
-      ip = request.getRemoteAddr();
-    }
-    return ip != null ? ip : "0.0.0.0";
+    return Arrays.stream(IpHeaders.values())
+        .map(ipHeader -> ipHeader.getIp(request))
+        .filter(Objects::nonNull)
+        .findFirst()
+        .orElse("0.0.0.0");
   }
 
   private static HttpServletRequest getHttpServletRequestByContext() {
     return ((ServletRequestAttributes) Objects.requireNonNull(
         RequestContextHolder.getRequestAttributes()))
         .getRequest();
+  }
+
+  private enum IpHeaders {
+    X_FORWARDED_FOR((request) -> request.getHeader("X-FORWARDED-FOR")),
+    PROXY_CLIENT_IP((request) -> request.getHeader("Proxy-Client-IP")),
+    WL_PROXY_CLIENT_IP((request) -> request.getHeader("WL-Proxy-Client-IP")),
+    HTTP_CLIENT_IP((request) -> request.getHeader("HTTP_CLIENT_IP")),
+    HTTP_X_FORWARDED_FOR((request) -> request.getHeader("HTTP_X_FORWARDED_FOR")),
+    REMOTE_ADDRESS((ServletRequest::getRemoteAddr));
+
+    private final Function<HttpServletRequest, String> getIpFunction;
+
+    IpHeaders(Function<HttpServletRequest, String> getIpFunction) {
+      this.getIpFunction = getIpFunction;
+    }
+
+    public String getIp(HttpServletRequest request) {
+      return getIpFunction.apply(request);
+    }
   }
 }
