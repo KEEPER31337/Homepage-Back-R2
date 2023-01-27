bc78dd2a2ab1a89cf237bbd118574081c6800aca
정핸모 <gusah009@naver.com>
2023-01-28 08:39:19 +0900

    fix: Error Code 수정

--- src/main/java/com/keeper/homepage/global/error/ErrorCode.java
+++ src/main/java/com/keeper/homepage/global/error/ErrorCode.java
@@ -11,8 +11,8 @@ public enum ErrorCode {
 
   // AUTH
   TOKEN_NOT_AVAILABLE("유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED),
-  AUTH_CODE_EXPIRED("인증 코드가 만료되었습니다.", HttpStatus.UNAUTHORIZED),
-  AUTH_CODE_MISMATCH("인증 코드가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED),
+  AUTH_CODE_EXPIRED("인증 코드가 없거나 만료되었습니다.", HttpStatus.NOT_FOUND),
+  AUTH_CODE_MISMATCH("인증 코드가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
   // MEMBER
   MEMBER_NOT_FOUND("해당 회원을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   MEMBER_EMAIL_DUPLICATE("회원 이메일이 중복됩니다.", HttpStatus.CONFLICT),
