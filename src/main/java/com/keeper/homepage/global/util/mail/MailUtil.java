e273c361a9a807415bed4279843d5dcd234d853b
정핸모 <gusah009@naver.com>
2023-01-27 14:48:37 +0900

    fix: @Service -> @Component

--- src/main/java/com/keeper/homepage/global/util/mail/MailUtil.java
+++ src/main/java/com/keeper/homepage/global/util/mail/MailUtil.java
@@ -4,9 +4,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class MailUtil {
 
