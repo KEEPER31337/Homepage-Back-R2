c7d4374ee4c908bb9de2c87e04285abb2a70f7d6
shkisme <shkisme0130@gmail.com>
2023-02-01 00:01:10 +0900

    test: 타입으로 페이지 블럭 리스트 조회 Test 코드 작성

--- src/test/java/com/keeper/homepage/domain/about/application/StaticWriteServiceTest.java
+++ src/test/java/com/keeper/homepage/domain/about/application/StaticWriteServiceTest.java
@@ -1,11 +1,15 @@
 package com.keeper.homepage.domain.about.application;
 
+import static com.keeper.homepage.domain.about.entity.StaticWriteTitle.StaticWriteTitleType.ACTIVITY;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.keeper.homepage.IntegrationTest;
+import com.keeper.homepage.domain.about.dto.response.StaticWriteTitleResponse;
 import com.keeper.homepage.domain.about.entity.StaticWriteTitle.StaticWriteTitleType;
+import com.keeper.homepage.global.error.BusinessException;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,5 +30,24 @@ public class StaticWriteServiceTest extends IntegrationTest {
     for (int i = 0; i < staticWriteTitleTypes.size(); ++i) {
       assertThat(response.get(i)).isEqualTo(staticWriteTitleTypes.get(i));
     }
+  }
+
+  @Test
+  @DisplayName("각 타이틀 타입으로 페이지 블럭 조회 시 성공적으로 조회되어야 한다.")
+  void should_getTitlesSuccessfully_when_getTitlesByType() {
+    StaticWriteTitleType type = ACTIVITY;
+    StaticWriteTitleResponse response = staticWriteService.getAllByType(type.getType());
+
+    assertThat(response.getId()).isEqualTo(type.getId());
+    assertThat(response.getTitle()).isEqualTo(type.getTitle());
+    assertThat(response.getType()).isEqualTo(type.getType());
+  }
+
+  @Test
+  @DisplayName("DB에서 찾을 수 없는 타입으로 타이틀 조회 시 Exception을 발생시킨다.")
+  void should_throwException_when_getTitlesByNotFoundType() {
+    assertThatThrownBy(() -> staticWriteService.getAllByType("null"))
+        .isInstanceOf(BusinessException.class)
+        .hasMessageContaining("해당 타입의 타이틀을 찾을 수 없습니다.");
   }
 }
