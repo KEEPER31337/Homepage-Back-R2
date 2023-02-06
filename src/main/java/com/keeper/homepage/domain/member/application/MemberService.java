6f02c516f23d9044816d0a8fbc9fa0e9cdc3addc
정핸모 <gusah009@naver.com>
2023-02-06 18:19:14 +0900

    feat: @Transactional(readonly = true) 추가

--- src/main/java/com/keeper/homepage/domain/member/application/MemberService.java
+++ src/main/java/com/keeper/homepage/domain/member/application/MemberService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
   @Transactional
