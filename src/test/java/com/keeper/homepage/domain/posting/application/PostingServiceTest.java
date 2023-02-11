bd4e8e85ee32da615acb9669008b6096ef8233dc
shkisme <shkisme0130@gmail.com>
2023-02-11 22:14:24 +0900

    fix: 게시글 생성 Service 테스트 자잘한 수정

--- src/test/java/com/keeper/homepage/domain/posting/application/PostingServiceTest.java
+++ src/test/java/com/keeper/homepage/domain/posting/application/PostingServiceTest.java
@@ -10,38 +10,29 @@ import com.keeper.homepage.domain.posting.entity.Posting;
 import com.keeper.homepage.domain.posting.entity.category.Category;
 import com.keeper.homepage.global.error.BusinessException;
 import com.keeper.homepage.global.util.web.WebUtil;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
 public class PostingServiceTest extends IntegrationTest {
 
   private Member member;
   private Category category;
+  private MockMultipartFile thumbnail;
 
   @BeforeEach
   void setUp() {
     member = memberTestHelper.generate();
     category = categoryTestHelper.generate();
+    thumbnail = thumbnailTestHelper.getThumbnailFile();
   }
 
   @Nested
   @DisplayName("게시글 생성")
   class CreatePost {
-
-    private final MockMultipartFile validMultipartFile = new MockMultipartFile("image",
-        "testImage_210x210.png", "image/png",
-        new FileInputStream("src/test/resources/images/testImage_210x210.png"));
-
-    CreatePost() throws IOException {
-    }
 
     @Test
     @DisplayName("게시글을 생성하면 성공적으로 저장된다.")
@@ -55,6 +46,8 @@ public class PostingServiceTest extends IntegrationTest {
           .isTemp(true)
           .password("게시글 암호")
           .categoryId(category.getId())
+          .thumbnail(thumbnail)
+          .files(List.of(thumbnail))
           .build();
 
       Long postId = postingService.createPost(member, request);
@@ -118,7 +111,7 @@ public class PostingServiceTest extends IntegrationTest {
       PostRequest request = PostRequest.builder()
           .title("게시글 제목")
           .content("게시글 내용")
-          .categoryId(999L)
+          .categoryId(0L)
           .build();
 
       assertThatThrownBy(() -> postingService.createPost(member, request))
