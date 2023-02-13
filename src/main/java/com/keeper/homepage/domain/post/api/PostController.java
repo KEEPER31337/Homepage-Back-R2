c80e977e4db7e1093502878525f1279bcddf69b3
shkisme <shkisme0130@gmail.com>
2023-02-13 11:50:30 +0900

    fix: url에 행위를 담지 않도록 수정

---
+++ src/main/java/com/keeper/homepage/domain/post/api/PostController.java
@@ -0,0 +1,46 @@
+package com.keeper.homepage.domain.post.api;
+
+import com.keeper.homepage.domain.member.entity.Member;
+import com.keeper.homepage.domain.post.application.PostService;
+import com.keeper.homepage.domain.post.dto.request.PostRequest;
+import com.keeper.homepage.global.config.security.annotation.LoginMember;
+import jakarta.validation.Valid;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+
+  private final PostService postService;
+
+  @PostMapping
+  public ResponseEntity<Void> createPost(
+      @LoginMember Member member,
+      @ModelAttribute @Valid PostRequest request
+  ) {
+    Long postId = postService.createPost(member, request);
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .location(URI.create("/posts/" + postId))
+        .build();
+  }
+
+  // TODO: 포스팅 생성 후 redirect용 api - 게시글 조회
+  @GetMapping("/{id}")
+  public void getPost(
+      @PathVariable("id") Long postId
+  ) {
+
+  }
+
+}
