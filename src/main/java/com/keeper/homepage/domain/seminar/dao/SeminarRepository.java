67121c4b223516150fa31b1aba43d1dc2b06a315
Hyun Yi <hy00un__@naver.com>
2023-02-13 10:39:29 +0900

    fix: todo 추가

--- src/main/java/com/keeper/homepage/domain/seminar/dao/SeminarRepository.java
+++ src/main/java/com/keeper/homepage/domain/seminar/dao/SeminarRepository.java
@@ -6,5 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeminarRepository extends JpaRepository<Seminar, Long> {
+
+  // TODO: 2023-02-13 List로 받아서 러프하게 할지, 하나만 받을지 고민해보기 
   Optional<Seminar> findByOpenTimeBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
