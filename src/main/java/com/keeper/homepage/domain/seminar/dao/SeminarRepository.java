90ca32a911cb9ff7d8046fd668c48da46aa96b9a
Hyun Yi <hy00un__@naver.com>
2023-02-14 04:36:07 +0900

    feat: `findAllByIdIsNot` 인터페이스 생성 및 `findAll`을 default 메서드로 재정의
    http://stackoverflow.com/questions/2430756/why-are-interface-variables-static-and-final-by-default
    인터페이스에 선언되는 변수는 static final로 정의된다.
    
    일반적으로 findAll 메서드를 사용하여 조회하는데, virtualSeminar 정보는 조회하지 않기 때문에 findAll을 default 메서드로 재정의했다.

--- src/main/java/com/keeper/homepage/domain/seminar/dao/SeminarRepository.java
+++ src/main/java/com/keeper/homepage/domain/seminar/dao/SeminarRepository.java
@@ -1,8 +1,17 @@
 package com.keeper.homepage.domain.seminar.dao;
 
 import com.keeper.homepage.domain.seminar.entity.Seminar;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeminarRepository extends JpaRepository<Seminar, Long> {
 
+  long virtualSeminarId = 1L;
+
+  @Override
+  default List<Seminar> findAll() {
+    return findAllByIdIsNot(virtualSeminarId);
+  }
+
+  List<Seminar> findAllByIdIsNot(Long seminarId);
 }
