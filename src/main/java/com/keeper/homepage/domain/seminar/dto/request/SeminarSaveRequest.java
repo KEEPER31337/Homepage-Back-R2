c63f648734ea13c6cf6b43222d0e9a56deee6232
Hyun Yi <hy00un__@naver.com>
2023-02-13 18:45:22 +0900

    fix: `@Nullable`, `@JsonFormat` 어노테이션 추가

--- src/main/java/com/keeper/homepage/domain/seminar/dto/request/SeminarSaveRequest.java
+++ src/main/java/com/keeper/homepage/domain/seminar/dto/request/SeminarSaveRequest.java
@@ -4,12 +4,14 @@ import static java.util.stream.Collectors.joining;
 import static lombok.AccessLevel.PACKAGE;
 import static lombok.AccessLevel.PRIVATE;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.keeper.homepage.domain.seminar.entity.Seminar;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
 @Getter
 @Builder
@@ -17,7 +19,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = PACKAGE)
 public class SeminarSaveRequest {
 
+  @Nullable
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime attendanceCloseTime;
+
+  @Nullable
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime latenessCloseTime;
 
   public Seminar toEntity(String attendanceCode) {
