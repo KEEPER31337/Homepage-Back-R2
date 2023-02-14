b3eec4a09331cc688ccd60242fe40c17f3156966
XETA <csh4519@naver.com>
2023-02-14 21:51:29 +0900

    fix: Study Entity 수정 - BaseEntity 상속 - 생성자 접근 제어자 수준 `@NoArgsConstructor(access = AccessLevel.PROTECTED)`로 조정 - cascade 적용 - length 숫자를 상수로 표현

--- src/main/java/com/keeper/homepage/domain/study/entity/Study.java
+++ src/main/java/com/keeper/homepage/domain/study/entity/Study.java
@@ -3,6 +3,8 @@ package com.keeper.homepage.domain.study.entity;
 
 import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.domain.thumbnail.entity.Thumbnail;
+import com.keeper.homepage.global.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,39 +15,40 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
+@DynamicInsert
+@DynamicUpdate
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "study")
-public class Study {
+public class Study extends BaseEntity {
+
+  private static final int MAX_TITLE_LENGTH = 45;
+  private static final int MAX_INFORMATION_LENGTH = 256;
+  private static final int MAX_LINK_LENGTH = 256;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", nullable = false, updatable = false)
   private Long id;
 
-  @Column(name = "title", nullable = false, length = 45)
+  @Column(name = "title", nullable = false, length = MAX_TITLE_LENGTH)
   private String title;
 
-  @Column(name = "information", nullable = false, length = 256)
+  @Column(name = "information", nullable = false, length = MAX_INFORMATION_LENGTH)
   private String information;
 
   @Column(name = "member_number", nullable = false)
   private Integer memberNumber;
-
-  @Column(name = "register_time", nullable = false)
-  private LocalDateTime registerTime;
-
-  @Column(name = "update_time", nullable = false)
-  private LocalDateTime updateTime;
 
   @Column(name = "year")
   private Integer year;
@@ -53,13 +56,13 @@ public class Study {
   @Column(name = "season")
   private Integer season;
 
-  @Column(name = "git_link", length = 256)
+  @Column(name = "git_link", length = MAX_LINK_LENGTH)
   private String gitLink;
 
-  @Column(name = "note_link", length = 256)
+  @Column(name = "note_link", length = MAX_LINK_LENGTH)
   private String noteLink;
 
-  @Column(name = "etc_link", length = 256)
+  @Column(name = "etc_link", length = MAX_LINK_LENGTH)
   private String etcLink;
 
   @ManyToOne(fetch = FetchType.LAZY)
@@ -70,19 +73,16 @@ public class Study {
   @JoinColumn(name = "head_member_id", nullable = false)
   private Member headMember;
 
-  @OneToMany(mappedBy = "studyHasMemberPK.study", orphanRemoval = true)
+  @OneToMany(mappedBy = "study", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<StudyHasMember> studyHasMember = new ArrayList<>();
 
   @Builder
   private Study(String title, String information, Integer memberNumber,
-      LocalDateTime registerTime, LocalDateTime updateTime,
       Integer year, Integer season, String gitLink, String noteLink, String etcLink,
-      Thumbnail thumbnail, Member headMember, List<StudyHasMember> studyHasMember) {
+      Thumbnail thumbnail, Member headMember) {
       this.title = title;
       this.information = information;
       this.memberNumber = memberNumber;
-      this.registerTime = registerTime;
-      this.updateTime = updateTime;
       this.year = year;
       this.season = season;
       this.gitLink = gitLink;
@@ -90,6 +90,5 @@ public class Study {
       this.etcLink = etcLink;
       this.thumbnail = thumbnail;
       this.headMember = headMember;
-      this.studyHasMember = studyHasMember;
   }
 }
