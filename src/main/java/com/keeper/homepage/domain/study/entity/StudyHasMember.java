2a32bfa177c83c7bbebe42a9a21daafea10f48c3
XETA <csh4519@naver.com>
2023-02-14 21:57:51 +0900

    fix: StudyHasMember Entity 수정 - 생성자 접근 제어자 수준 조정 - 매핑 문제로 EmbeddedId에서 IdClass로 수정

--- src/main/java/com/keeper/homepage/domain/study/entity/StudyHasMember.java
+++ src/main/java/com/keeper/homepage/domain/study/entity/StudyHasMember.java
@@ -1,32 +1,42 @@
 package com.keeper.homepage.domain.study.entity;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 import com.keeper.homepage.domain.member.entity.Member;
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
+import com.keeper.homepage.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.io.Serializable;
-import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@NoArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+@IdClass(StudyHasMemberPK.class)
 @Table(name = "study_has_member")
-public class StudyHasMember implements Serializable {
+public class StudyHasMember extends BaseEntity {
 
-  @EmbeddedId
-  private StudyHasMemberPK studyHasMemberPK;
+  @Id
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "study_id", nullable = false, updatable = false)
+  private Study study;
 
-  @Column(name = "register_time", nullable = false, updatable = false)
-  private LocalDateTime registerTime;
+  @Id
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "member_id", nullable = false, updatable = false)
+  private Member member;
 
   @Builder
-  private StudyHasMember(StudyHasMemberPK studyHasMemberPK, LocalDateTime registerTime) {
-    this.studyHasMemberPK = studyHasMemberPK;
-    this.registerTime = registerTime;
+  private StudyHasMember(Study study, Member member) {
+    this.study = study;
+    this.member = member;
   }
 }
--- src/main/java/com/keeper/homepage/domain/study/entity/StudyHasMemberPK.java
+++ src/main/java/com/keeper/homepage/domain/study/entity/StudyHasMemberPK.java
@@ -1,27 +1,11 @@
 package com.keeper.homepage.domain.study.entity;
 
-import com.keeper.homepage.domain.member.entity.Member;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import java.io.Serializable;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-
-@Embeddable
 @Getter
-@NoArgsConstructor
-@EqualsAndHashCode
 public class StudyHasMemberPK implements Serializable {
 
-  @ManyToOne(targetEntity = Study.class, fetch = FetchType.LAZY)
-  @JoinColumn(name = "study_id", nullable = false, updatable = false)
-  private Study study;
-
-  @ManyToOne(targetEntity = Member.class, fetch = FetchType.LAZY)
-  @JoinColumn(name = "member_id", nullable = false, updatable = false)
-  private Member member;
+  private Long study;
+  private Long member;
 }
