d01ae1f80fa0d29d3603ac090142cba09a83d7bb
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-02-14 23:26:44 +0900

    fix: GameRepositoryTest 에 GameTestHelper 적용 및 timeTest 수정

--- src/test/java/com/keeper/homepage/domain/game/dao/GameRepositoryTest.java
+++ src/test/java/com/keeper/homepage/domain/game/dao/GameRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.keeper.homepage.domain.game.dao;
 
-import static java.time.LocalDateTime.*;
 import static org.assertj.core.api.Assertions.*;
 
 import com.keeper.homepage.IntegrationTest;
@@ -8,7 +7,6 @@ import com.keeper.homepage.domain.game.entity.Game;
 import com.keeper.homepage.domain.member.entity.Member;
 import java.time.LocalDateTime;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -18,23 +16,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 class GameRepositoryTest extends IntegrationTest {
 
-    private Member member;
-
-    @BeforeEach
-    void setUp() {
-        member = memberTestHelper.generate();
-    }
-
     @Nested
     @DisplayName("DB Default 테스트")
     class Default {
 
         @Test
         @DisplayName("Game 엔티티 저장")
-        public void checkDefault() throws Exception {
+        public void saveGame() throws Exception {
             // given
-            Game game = Game.builder()
-                .member(member)
+            Game game = gameTestHelper
+                .builder()
                 .dicePerDay(0)
                 .lottoPerDay(0)
                 .roulettePerDay(0)
@@ -53,7 +44,6 @@ class GameRepositoryTest extends IntegrationTest {
             assertThat(findGame.get().getDicePerDay()).isEqualTo(0);
             assertThat(findGame.get().getLottoPerDay()).isEqualTo(0);
             assertThat(findGame.get().getRoulettePerDay()).isEqualTo(0);
-
             assertThat(findGame.get().getDiceDayPoint()).isEqualTo(10000);
             assertThat(findGame.get().getLottoDayPoint()).isEqualTo(20000);
             assertThat(findGame.get().getRouletteDayPoint()).isEqualTo(30000);
@@ -63,15 +53,7 @@ class GameRepositoryTest extends IntegrationTest {
         @DisplayName("Game 엔티티 삭제")
         public void DeleteGame() throws Exception {
             // given
-            Game game = Game.builder()
-                .member(member)
-                .dicePerDay(0)
-                .lottoPerDay(0)
-                .roulettePerDay(0)
-                .diceDayPoint(10000)
-                .lottoDayPoint(20000)
-                .rouletteDayPoint(30000)
-                .build();
+            Game game = gameTestHelper.generate();
 
             // when
             Long savedId = gameRepository.save(game).getId();
@@ -88,28 +70,20 @@ class GameRepositoryTest extends IntegrationTest {
         @DisplayName("마지막 플레이 시간은 DB에서 저장된다.")
         public void timeTest() throws Exception {
             // given
-            Game game = Game.builder()
-                .member(member)
-                .dicePerDay(0)
-                .lottoPerDay(0)
-                .roulettePerDay(0)
-                .diceDayPoint(10000)
-                .lottoDayPoint(20000)
-                .rouletteDayPoint(30000)
-                .build();
+            Game game = gameTestHelper.generate(); //build 될 때, lastPlayTime은 null이 들어감.
 
             //when
-            LocalDateTime beforeSaveTime = now();
+            LocalDateTime SavedTime = LocalDateTime.now().minusSeconds(1);
             Long savedId = gameRepository.save(game).getId();
             em.flush();
             em.clear();
             Optional<Game> findGame = gameRepository.findById(savedId);
+            LocalDateTime findTime = LocalDateTime.now().plusSeconds(1);
 
-            // then
-            assertThat(findGame.get().getLastPlayTime()).isBefore(beforeSaveTime);
+            //then
+            assertThat(findGame.get().getLastPlayTime()).isAfter(SavedTime);
+            assertThat(findGame.get().getLastPlayTime()).isBefore(findTime);
         }
-
-
     }
 
     @Nested
@@ -120,15 +94,7 @@ class GameRepositoryTest extends IntegrationTest {
         @DisplayName("주사위, 룰렛, 로또 게임 횟수 증가")
         public void increaseGameTimesTest() throws Exception {
             // given
-            Game game = Game.builder()
-                .member(member)
-                .dicePerDay(0)
-                .lottoPerDay(0)
-                .roulettePerDay(0)
-                .diceDayPoint(10000)
-                .lottoDayPoint(20000)
-                .rouletteDayPoint(30000)
-                .build();
+            Game game = gameTestHelper.generate();
 
             // when
             Long savedId = gameRepository.save(game).getId();
@@ -157,11 +123,11 @@ class GameRepositoryTest extends IntegrationTest {
         @DisplayName("게임 필드 리셋")
         public void gameResetTest() throws Exception {
             // given
-            Game game = Game.builder()
-                .member(member)
-                .dicePerDay(0)
-                .lottoPerDay(0)
-                .roulettePerDay(0)
+            Game game = gameTestHelper
+                .builder()
+                .dicePerDay(3)
+                .lottoPerDay(3)
+                .roulettePerDay(3)
                 .diceDayPoint(10000)
                 .lottoDayPoint(20000)
                 .rouletteDayPoint(30000)
@@ -180,7 +146,6 @@ class GameRepositoryTest extends IntegrationTest {
             assertThat(resetedGame.getDicePerDay()).isEqualTo(0);
             assertThat(resetedGame.getLottoPerDay()).isEqualTo(0);
             assertThat(resetedGame.getRoulettePerDay()).isEqualTo(0);
-
             assertThat(resetedGame.getDiceDayPoint()).isEqualTo(0);
             assertThat(resetedGame.getLottoDayPoint()).isEqualTo(0);
             assertThat(resetedGame.getRouletteDayPoint()).isEqualTo(0);
