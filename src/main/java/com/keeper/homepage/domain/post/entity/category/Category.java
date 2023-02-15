b93efc408f770f23cf494aa6fbe43c1fcaef5b06
shkisme <shkisme0130@gmail.com>
2023-02-15 09:00:55 +0900

    test: 카테고리가 게시글의 삭제 주기를 관리하지 않도록 수정

--- src/main/java/com/keeper/homepage/domain/post/entity/category/Category.java
+++ src/main/java/com/keeper/homepage/domain/post/entity/category/Category.java
@@ -41,7 +41,7 @@ public class Category {
   @Column(name = "href", length = MAX_HREF_LENGTH)
   private String href;
 
-  @OneToMany(mappedBy = "category", orphanRemoval = true)
+  @OneToMany(mappedBy = "category")
   private final List<Post> posts = new ArrayList<>();
 
   @OneToMany(mappedBy = "parent", cascade = ALL, orphanRemoval = true)
@@ -55,7 +55,7 @@ public class Category {
   }
 
   public void addPost(Post post) {
-    post.assignCategory(this);
+    post.registerCategory(this);
     posts.add(post);
   }
 
--- src/test/java/com/keeper/homepage/domain/post/dao/category/CategoryRepositoryTest.java
+++ src/test/java/com/keeper/homepage/domain/post/dao/category/CategoryRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.keeper.homepage.IntegrationTest;
 import com.keeper.homepage.domain.member.entity.Member;
-import com.keeper.homepage.domain.post.entity.Post;
 import com.keeper.homepage.domain.post.entity.category.Category;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -29,7 +28,7 @@ public class CategoryRepositoryTest extends IntegrationTest {
     @Test
     @DisplayName("부모 카테고리에 자식 카테고리를 등록하면 DB에 저장되어야 한다.")
     void should_saveChildCategory_when_parentAddChild() {
-      // TODO: DB가 변경되면 성공해야 합니다.
+      // TODO: [DB] self referencing 하는 foreign key로 설정되면 변경한다.
     }
   }
 
@@ -38,20 +37,9 @@ public class CategoryRepositoryTest extends IntegrationTest {
   class CategoryRemoveTest {
 
     @Test
-    @DisplayName("카테고리를 지우면 카테고리의 포스팅 글들도 지워진다.")
-    void should_deletedPosts_when_deleteCategory() {
-      Post post = postTestHelper.generate();
-      category.addPost(post);
-
-      categoryRepository.delete(category);
-
-      assertThat(postRepository.findAll()).doesNotContain(post);
-    }
-
-    @Test
     @DisplayName("부모 카테고리를 지우면 자식 카테고리들도 지워진다.")
     void should_deletedChildren_when_deleteParent() {
-      // TODO: DB가 변경되면 성공해야 합니다.
+      // TODO: [DB] self referencing 하는 foreign key로 설정되면 변경한다.
     }
   }
 
@@ -61,6 +49,7 @@ public class CategoryRepositoryTest extends IntegrationTest {
 
     @Test
     @DisplayName("부모 카테고리를 넣지 않았을 때 0L으로 처리해야 한다.")
+      // TODO: [DB] 부모 카테고리가 없을 경우 null로 처리하도록 변경되면 수정한다.
     void should_processDefault_when_EmptyParentCategory() {
       em.flush();
       em.clear();
