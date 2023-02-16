556abf7c8fc3df6853b971aee0656eebf1da17f4
Hyun Yi <hy00un__@naver.com>
2023-02-16 18:52:36 +0900

    fix: `findByOpenTime`에 Optional 추가
    Optional이 필요한 이유는 정상적인 입력 값이지만, `findByOpenTime` 명령어를 사용할 때 데이터가 없으면 예외가 발생하기 때문에 (Seminar 타입으로 반환할 데이터가 없기 때문) 이를 컨트롤하기 위함이다.

--- src/main/java/com/keeper/homepage/domain/seminar/dao/SeminarRepository.java
+++ src/main/java/com/keeper/homepage/domain/seminar/dao/SeminarRepository.java
@@ -3,6 +3,7 @@ package com.keeper.homepage.domain.seminar.dao;
 import com.keeper.homepage.domain.seminar.entity.Seminar;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 public interface SeminarRepository extends JpaRepository<Seminar, Long> {
 
   List<Seminar> findAllByIdIsNot(Long seminarId);
-  
-  @Query("SELECT s FROM Seminar s WHERE DATE(s.openTime) =:date")
-  Seminar findByOpenTime(@Param("date") LocalDate date);
+
+  @Query("SELECT s FROM Seminar s WHERE DATE(s.openTime) =:localDate")
+  Optional<Seminar> findByOpenTime(@Param("localDate") LocalDate localDate);
 }
