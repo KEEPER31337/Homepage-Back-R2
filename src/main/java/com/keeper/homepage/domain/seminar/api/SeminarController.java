bd3eabf3750f976e595d7610e3bbd864de945dc6
Hyun Yi <hy00un__@naver.com>
2023-02-16 18:42:38 +0900

    feat: `ResponseEntity`를 사용하여 HTTP 상태 코드 세밀화

--- src/main/java/com/keeper/homepage/domain/seminar/api/SeminarController.java
+++ src/main/java/com/keeper/homepage/domain/seminar/api/SeminarController.java
@@ -1,14 +1,13 @@
 package com.keeper.homepage.domain.seminar.api;
 
 import com.keeper.homepage.domain.seminar.application.SeminarService;
-import com.keeper.homepage.domain.seminar.application.convenience.FindService;
 import com.keeper.homepage.domain.seminar.dto.request.SeminarSaveRequest;
 import com.keeper.homepage.domain.seminar.dto.response.SeminarResponse;
-import java.time.LocalDate;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,30 +25,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class SeminarController {
 
   private final SeminarService seminarService;
-  private final FindService findService;
 
   @PostMapping
-  public Long createSeminar(@Valid @RequestBody SeminarSaveRequest request) {
-    return seminarService.save(request);
+  public ResponseEntity<Long> createSeminar(@Valid @RequestBody SeminarSaveRequest request) {
+    Long seminarId = seminarService.save(request);
+    return ResponseEntity.status(HttpStatus.OK).body(seminarId);
   }
 
   @DeleteMapping("/{seminarId}")
-  public void deleteSeminar(@PathVariable long seminarId) {
+  public ResponseEntity<Void> deleteSeminar(@PathVariable long seminarId) {
     seminarService.delete(seminarId);
+    return ResponseEntity.noContent().build();
   }
 
   @GetMapping
-  public List<SeminarResponse> getAllSeminars() {
-    return findService.findAll();
+  public ResponseEntity<List<SeminarResponse>> getAllSeminars() {
+    List<SeminarResponse> seminarList = seminarService.findAll();
+    return ResponseEntity.status(HttpStatus.OK).body(seminarList);
   }
 
   @GetMapping(params = "date")
-  public SeminarResponse getSeminarByDate(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
-    return seminarService.findByDate(date);
+  public ResponseEntity<SeminarResponse> getSeminarByDate(
+      @RequestParam("date") String strLocalDate) {
+    SeminarResponse seminarResponse = seminarService.findByDate(strLocalDate);
+    return ResponseEntity.status(HttpStatus.OK).body(seminarResponse);
   }
 
   @GetMapping("/{seminarId}")
-  public SeminarResponse getSeminar(@PathVariable long seminarId) {
-    return seminarService.findById(seminarId);
+  public ResponseEntity<SeminarResponse> getSeminar(@PathVariable long seminarId) {
+    SeminarResponse seminarResponse = seminarService.findById(seminarId);
+    return ResponseEntity.status(HttpStatus.OK).body(seminarResponse);
   }
 }
