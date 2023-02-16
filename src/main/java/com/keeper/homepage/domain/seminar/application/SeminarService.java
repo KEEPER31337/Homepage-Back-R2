db5abfa698b66d3da2e5bc91a31954ca921b8e66
Hyun Yi <hy00un__@naver.com>
2023-02-16 18:48:55 +0900

    fix: 문자로 받은 날짜 값을 검증 후 매핑하는 방법으로 수정
    Controller에서 `@LocalDateFormat`을 사용하면 지정된 데이터 형식으로는 매핑이 되지만, 올바르지 않은 값이 들어왔을 때 LocalDate로 매핑이 안되기 때문에 HTTP 상태 코드를 제어하지 못하고 500 에러가 발생하게 됩니다.
    
    GET 파라미터로 들어온 문자 형식의 날짜 값을 검증 후에 LocalDate로 매핑하는 방법으로 수정하였습니다.

--- src/main/java/com/keeper/homepage/domain/seminar/application/SeminarService.java
+++ src/main/java/com/keeper/homepage/domain/seminar/application/SeminarService.java
@@ -3,6 +3,7 @@ package com.keeper.homepage.domain.seminar.application;
 import static com.keeper.homepage.global.error.ErrorCode.SEMINAR_NOT_FOUND;
 import static com.keeper.homepage.global.error.ErrorCode.SEMINAR_TIME_NOT_AVAILABLE;
 
+import com.keeper.homepage.domain.seminar.application.convenience.FindService;
 import com.keeper.homepage.domain.seminar.dao.SeminarRepository;
 import com.keeper.homepage.domain.seminar.dto.request.SeminarSaveRequest;
 import com.keeper.homepage.domain.seminar.dto.response.SeminarResponse;
@@ -10,6 +11,7 @@ import com.keeper.homepage.domain.seminar.entity.Seminar;
 import com.keeper.homepage.global.error.BusinessException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class SeminarService {
 
   private final SeminarRepository seminarRepository;
+  private final FindService findService;
 
   @Transactional
   public Long save(SeminarSaveRequest request) {
@@ -51,7 +54,24 @@ public class SeminarService {
         .orElseThrow(() -> new BusinessException(seminarId, "seminarId", SEMINAR_NOT_FOUND)));
   }
 
-  public SeminarResponse findByDate(LocalDate localDate) {
-    return new SeminarResponse(seminarRepository.findByOpenTime(localDate));
+  public SeminarResponse findByDate(String localDate) {
+    validLocalDate(localDate);
+
+    LocalDate parseDate = LocalDate.parse(localDate);
+    return new SeminarResponse(seminarRepository.findByOpenTime(parseDate)
+        .orElse(Seminar.builder().build()));
+  }
+
+  private void validLocalDate(String localDate) {
+    try {
+      LocalDate.parse(localDate);
+    } catch (Exception e) {
+      throw new BusinessException(localDate, "localDate",
+          SEMINAR_TIME_NOT_AVAILABLE);
+    }
+  }
+
+  public List<SeminarResponse> findAll() {
+    return findService.findAll();
   }
 }
