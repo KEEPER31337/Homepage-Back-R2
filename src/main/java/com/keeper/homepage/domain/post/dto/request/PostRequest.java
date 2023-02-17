ed872dd9db8e7697c24b272aad7d916d46a360a2
shkisme <shkisme0130@gmail.com>
2023-02-17 21:52:17 +0900

    style: @Size 어노테이션 사용하여 에러 메세지 간결하게 수정

--- src/main/java/com/keeper/homepage/domain/post/dto/request/PostRequest.java
+++ src/main/java/com/keeper/homepage/domain/post/dto/request/PostRequest.java
@@ -1,7 +1,5 @@
 package com.keeper.homepage.domain.post.dto.request;
 
-import static com.keeper.homepage.domain.post.entity.Post.MAX_PASSWORD_LENGTH;
-import static com.keeper.homepage.domain.post.entity.Post.MAX_TITLE_LENGTH;
 import static lombok.AccessLevel.PRIVATE;
 
 import com.keeper.homepage.domain.member.entity.Member;
@@ -11,6 +9,7 @@ import com.keeper.homepage.domain.thumbnail.entity.Thumbnail;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,8 +27,7 @@ public class PostRequest {
   public static final int MAX_REQUEST_PASSWORD_LENGTH = 16;
 
   @NotBlank(message = "게시글 제목을 입력해주세요.")
-  @Length(max = MAX_REQUEST_TITLE_LENGTH, message = "게시글 제목은 " + MAX_REQUEST_TITLE_LENGTH
-      + "자 이하로 입력해주세요.")
+  @Size(max = MAX_REQUEST_TITLE_LENGTH, message = "게시글 제목은 {max}자 이하로 입력해주세요.")
   private String title;
 
   @NotBlank(message = "게시글 본문을 입력해주세요.")
@@ -48,8 +46,7 @@ public class PostRequest {
   private Boolean isTemp;
 
   @Nullable
-  @Length(max = MAX_REQUEST_PASSWORD_LENGTH, message = "비밀번호는 " + MAX_REQUEST_PASSWORD_LENGTH
-      + "자 이하로 입력해주세요.")
+  @Size(max = MAX_REQUEST_PASSWORD_LENGTH, message = "비밀번호는 {max}자 이하로 입력해주세요.")
   private String password;
 
   @NotNull(message = "카테고리 아이디를 입력해주세요.")
