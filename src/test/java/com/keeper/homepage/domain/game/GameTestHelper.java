6442c01248d50b6b70664128aa8f56537bf975c4
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-20 18:15:08 +0900

    refactor : 값객체로 생성으로 인해 GameTestHelper코드 수정

--- src/test/java/com/keeper/homepage/domain/game/GameTestHelper.java
+++ src/test/java/com/keeper/homepage/domain/game/GameTestHelper.java
@@ -2,6 +2,9 @@ package com.keeper.homepage.domain.game;
 
 import com.keeper.homepage.domain.game.dao.GameRepository;
 import com.keeper.homepage.domain.game.entity.Game;
+import com.keeper.homepage.domain.game.entity.embedded.Dice;
+import com.keeper.homepage.domain.game.entity.embedded.Lotto;
+import com.keeper.homepage.domain.game.entity.embedded.Roulette;
 import com.keeper.homepage.domain.member.MemberTestHelper;
 import com.keeper.homepage.domain.member.entity.Member;
 
@@ -12,85 +15,63 @@ import org.springframework.stereotype.Component;
 @Component
 public class GameTestHelper {
 
-    @Autowired
-    GameRepository gameRepository;
+  @Autowired
+  GameRepository gameRepository;
 
-    @Autowired
-    MemberTestHelper memberTestHelper;
+  @Autowired
+  MemberTestHelper memberTestHelper;
 
-    public Game generate() {
-        return this.builder().build();
+  public Game generate() {
+    return this.builder().build();
+  }
+
+  public GameBuilder builder() {
+    return new GameBuilder();
+  }
+
+  public final class GameBuilder {
+
+    private Member member;
+    private LocalDateTime lastPlayTime;
+    private Dice dice;
+    private Lotto lotto;
+    private Roulette roulette;
+
+    private GameBuilder() {
     }
 
-    public GameBuilder builder() {
-        return new GameBuilder();
+    public GameBuilder member(Member member) {
+      this.member = member;
+      return this;
     }
 
-    public final class GameBuilder {
-
-        private Member member;
-        private LocalDateTime lastPlayTime;
-        private Integer dicePerDay;
-        private Integer roulettePerDay;
-        private Integer lottoPerDay;
-        private Integer diceDayPoint;
-        private Integer rouletteDayPoint;
-        private Integer lottoDayPoint;
-
-        private GameBuilder() {
-        }
-
-        public GameBuilder member(Member member) {
-            this.member = member;
-            return this;
-        }
-
-        public GameBuilder lastPlayTime(LocalDateTime lastPlayTime) {
-            this.lastPlayTime = lastPlayTime;
-            return this;
-        }
-
-        public GameBuilder dicePerDay(Integer dicePerDay) {
-            this.dicePerDay = dicePerDay;
-            return this;
-        }
-
-        public GameBuilder roulettePerDay(Integer roulettePerDay) {
-            this.roulettePerDay = roulettePerDay;
-            return this;
-        }
-
-        public GameBuilder lottoPerDay(Integer lottoPerDay) {
-            this.lottoPerDay = lottoPerDay;
-            return this;
-        }
-
-        public GameBuilder diceDayPoint(Integer diceDayPoint) {
-            this.diceDayPoint = diceDayPoint;
-            return this;
-        }
-
-        public GameBuilder rouletteDayPoint(Integer rouletteDayPoint) {
-            this.rouletteDayPoint = rouletteDayPoint;
-            return this;
-        }
-
-        public GameBuilder lottoDayPoint(Integer lottoDayPoint) {
-            this.lottoDayPoint = lottoDayPoint;
-            return this;
-        }
-
-        public Game build() {
-            return gameRepository.save(Game.builder()
-                .member(member != null ? member : memberTestHelper.generate())
-                .lastPlayTime(null)
-                .dicePerDay(dicePerDay != null ? dicePerDay : 0)
-                .lottoPerDay(lottoPerDay != null ? lottoPerDay : 0)
-                .roulettePerDay(roulettePerDay != null ? roulettePerDay : 0)
-                .diceDayPoint(diceDayPoint != null ? diceDayPoint : 0)
-                .lottoDayPoint(lottoDayPoint != null ? lottoDayPoint : 0)
-                .rouletteDayPoint(rouletteDayPoint != null ? rouletteDayPoint : 0)
-                .build());
-        }
+    public GameBuilder lastPlayTime(LocalDateTime lastPlayTime) {
+      this.lastPlayTime = lastPlayTime;
+      return this;
     }
+
+    public GameBuilder dice(Dice dice) {
+      this.dice = dice;
+      return this;
+  }
+
+    public GameBuilder lotto(Lotto lotto) {
+      this.lotto = lotto;
+      return this;
+    }
+
+    public GameBuilder roulette(Roulette roulette) {
+      this.roulette = roulette;
+      return this;
+    }
+
+    public Game build() {
+      return gameRepository.save(
+          Game.builder().member(member != null ? member : memberTestHelper.generate())
+              .lastPlayTime(null)
+              .dice(dice != null ? dice : dice.from(0, 0))
+              .lotto(lotto != null ? lotto : lotto.from(0, 0))
+              .roulette(roulette != null ? roulette : roulette.from(0, 0)).build());
+    }
+  }
 }
