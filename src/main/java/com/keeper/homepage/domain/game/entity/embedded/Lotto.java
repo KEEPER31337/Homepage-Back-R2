de5b18889453bb4ac383362c6b291dbe48b98a36
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-20 18:14:16 +0900

    feat : Lotto 값 객체 생성

---
+++ src/main/java/com/keeper/homepage/domain/game/entity/embedded/Lotto.java
@@ -0,0 +1,39 @@
+package com.keeper.homepage.domain.game.entity.embedded;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class Lotto {
+
+  private static final int LOTTO_MAX_PLAYTIME = 3;
+
+  @Column(name = "lotto_per_day", nullable = false)
+  private Integer lottoPerDay;
+
+  @Column(name = "lotto_day_point", nullable = false)
+  private Integer lottoDayPoint;
+
+  public Lotto from(Integer lottoPerDay, Integer lottoDayPoint) {
+    return new Lotto(lottoPerDay, lottoDayPoint);
+  }
+
+  public void increaseLottoTimes() {
+    this.lottoPerDay += 1;
+  }
+
+  public void resetLottoPerDay() {
+    this.lottoPerDay = 0;
+  }
+
+  public void resetLottoDayPoint() {
+    this.lottoDayPoint = 0;
+  }
+}
