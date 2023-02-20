2f0e95ffd4ad8da5bcd87aeec03092ee23e18efe
geunskoo <oryukdo3@pusan.ac.kr>
2023-02-20 18:12:21 +0900

    feat : Dice 값 객체 생성

---
+++ src/main/java/com/keeper/homepage/domain/game/entity/embedded/Dice.java
@@ -0,0 +1,39 @@
+package com.keeper.homepage.domain.game.entity.embedded;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class Dice {
+
+  private static final int DICE_MAX_PLAYTIME = 3;
+
+  @Column(name = "dice_per_day", nullable = false)
+  private Integer dicePerDay;
+
+  @Column(name = "dice_day_point", nullable = false)
+  private Integer diceDayPoint;
+
+  public Dice from(Integer dicePerDay, Integer diceDayPoint) {
+    return new Dice(dicePerDay, diceDayPoint);
+  }
+
+  public void increaseDiceTimes() {
+    this.dicePerDay += 1;
+  }
+
+  public void resetDicePerDay() {
+    this.dicePerDay = 0;
+  }
+
+  public void resetDiceDayPoint() {
+    this.diceDayPoint = 0;
+  }
+}
