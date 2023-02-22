f3f65dfeb428b36eedfdfa07964f577510253d66
Hyun Yi <hy00un__@naver.com>
2023-02-22 22:47:33 +0900

    fix: response 클래스 `AccessLevel` 설정

--- src/main/java/com/keeper/homepage/domain/seminar/dto/response/SeminarAttendanceResponse.java
+++ src/main/java/com/keeper/homepage/domain/seminar/dto/response/SeminarAttendanceResponse.java
@@ -1,14 +1,17 @@
 package com.keeper.homepage.domain.seminar.dto.response;
 
+import static lombok.AccessLevel.PACKAGE;
 import static lombok.AccessLevel.PRIVATE;
 
 import com.keeper.homepage.domain.seminar.entity.SeminarAttendance;
 import com.keeper.homepage.domain.seminar.entity.SeminarAttendanceStatus.SeminarAttendanceStatusType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor(access = PRIVATE)
+@NoArgsConstructor(access = PRIVATE)
+@AllArgsConstructor(access = PACKAGE)
 public class SeminarAttendanceResponse {
 
   private Long id;
--- src/main/java/com/keeper/homepage/domain/seminar/dto/response/SeminarResponse.java
+++ src/main/java/com/keeper/homepage/domain/seminar/dto/response/SeminarResponse.java
@@ -1,13 +1,19 @@
 package com.keeper.homepage.domain.seminar.dto.response;
 
+import static lombok.AccessLevel.PACKAGE;
+import static lombok.AccessLevel.PRIVATE;
+
 import com.keeper.homepage.domain.seminar.entity.Seminar;
-import com.keeper.homepage.domain.seminar.entity.SeminarAttendance;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor(access = PRIVATE)
+@AllArgsConstructor(access = PACKAGE)
 public class SeminarResponse {
 
   // TODO: 2023-02-13 front-end와 상의 후 필요한 데이터만 선언하고 나머지는 삭제 예정
