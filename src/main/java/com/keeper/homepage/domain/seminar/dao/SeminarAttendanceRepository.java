b7224b449e7cdcde1fba0d2285a7034bccc36607
Hyun Yi <hy00un__@naver.com>
2023-02-22 19:48:32 +0900

    fix: 쿼리 메서드에 있는 `@Param` 제거

--- src/main/java/com/keeper/homepage/domain/seminar/dao/SeminarAttendanceRepository.java
+++ src/main/java/com/keeper/homepage/domain/seminar/dao/SeminarAttendanceRepository.java
@@ -4,9 +4,8 @@ import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.domain.seminar.entity.Seminar;
 import com.keeper.homepage.domain.seminar.entity.SeminarAttendance;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface SeminarAttendanceRepository extends JpaRepository<SeminarAttendance, Long> {
 
-  boolean existsBySeminarAndMember(@Param("seminar") Seminar seminar, @Param("member") Member member);
+  boolean existsBySeminarAndMember(Seminar seminar, Member member);
 }
