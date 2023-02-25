eda58bab30afd53cf7df0c55c603db3763fdffbc
shkisme <shkisme0130@gmail.com>
2023-02-25 21:23:28 +0900

    feat: ValidPostFindService 추가

---
+++ src/main/java/com/keeper/homepage/domain/post/application/convenience/ValidPostFindService.java
@@ -0,0 +1,31 @@
+package com.keeper.homepage.domain.post.application.convenience;
+
+import static com.keeper.homepage.global.error.ErrorCode.POST_NOT_FOUND;
+
+import com.keeper.homepage.domain.post.dao.PostRepository;
+import com.keeper.homepage.domain.post.entity.Post;
+import com.keeper.homepage.global.error.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ValidPostFindService {
+
+  private static final long VIRTUAL_POST_ID = 1;
+  private final PostRepository postRepository;
+
+  public Post findById(long postId) {
+    checkVirtualPostId(postId);
+    return postRepository.findById(postId)
+        .orElseThrow(() -> new BusinessException(postId, "postId", POST_NOT_FOUND));
+  }
+
+  private void checkVirtualPostId(long postId) {
+    if (postId == VIRTUAL_POST_ID) {
+      throw new BusinessException(postId, "postId", POST_NOT_FOUND);
+    }
+  }
+}
