0fdd2163b7617495653d09a3ce5cfc917054d7bd
정핸모 <gusah009@naver.com>
2023-02-26 13:16:14 +0900

    build: groovy -> kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,96 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.0.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.jpa") version "1.7.22"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+/******* Start Spring Rest Docs *******/
+
+val asciidoctorExt: Configuration by configurations.creating
+val snippetsDir by extra { "$buildDir/generated-snippets" }
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+    withType<Test> {
+        useJUnitPlatform()
+    }
+    test {
+        useJUnitPlatform()
+        outputs.dir(snippetsDir)
+    }
+    asciidoctor {
+        doFirst {
+            project.delete(files("src/main/resources/static/docs"))
+        }
+        inputs.dir(snippetsDir)
+        configurations("asciidoctorExt")
+        dependsOn(test)
+        baseDirFollowsSourceFile()
+    }
+    register<Copy>("copyDocs") {
+        dependsOn(asciidoctor)
+        from("${asciidoctor.get().outputDir}/html5")
+        into("src/main/resources/static/docs")
+    }
+    bootJar {
+        dependsOn(asciidoctor)
+        from("${asciidoctor.get().outputDir}/html5") {
+            into("static/docs")
+        }
+    }
+    build {
+        dependsOn("copyDocs")
+    }
+}
+/******* End Spring Rest Docs *******/
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.8.1")
+
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("org.springframework.security:spring-security-test")
+}
