33c7fe7badf94d1a0c548aac0b456d9e5d6e70b4
정핸모 <gusah009@naver.com>
2023-02-27 22:24:30 +0900

    feat: 책 부서 타입 enum을 request로 받기 위해 Converter 수정

--- src/main/java/com/keeper/homepage/global/config/web/WebMvcConfiguration.java
+++ src/main/java/com/keeper/homepage/global/config/web/WebMvcConfiguration.java
@@ -2,10 +2,12 @@ package com.keeper.homepage.global.config.web;
 
 import static com.keeper.homepage.global.util.file.server.FileServerConstants.RESOURCE_PATH;
 
+import com.keeper.homepage.domain.library.converter.BookDepartmentTypeConverter;
 import com.keeper.homepage.global.config.security.annotation.LoginMemberArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,5 +28,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     registry.addResourceHandler("/keeper_files/**")
         .addResourceLocations("file:" + RESOURCE_PATH)
         .setCachePeriod(3600);
+  }
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(new BookDepartmentTypeConverter());
   }
 }
