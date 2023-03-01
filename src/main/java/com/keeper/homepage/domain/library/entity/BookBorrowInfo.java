8e08c92a6511af4d7907a3c709955817bf7f942b
JIMIN LEE <min102602@naver.com>
2023-03-01 17:55:19 +0900

    fix: `status_id` 추가

--- src/main/java/com/keeper/homepage/domain/library/entity/BookBorrowInfo.java
+++ src/main/java/com/keeper/homepage/domain/library/entity/BookBorrowInfo.java
@@ -6,15 +6,8 @@ import static lombok.AccessLevel.PROTECTED;
 
 import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.global.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -44,6 +37,10 @@ public class BookBorrowInfo extends BaseEntity {
   @JoinColumn(name = "book_id", nullable = false)
   private Book book;
 
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "status_id", nullable = false)
+  private BookBorrowStatus borrowStatus;
+
   @Column(name = "borrow_date", nullable = false)
   private LocalDateTime borrowDate;
 
@@ -51,10 +48,11 @@ public class BookBorrowInfo extends BaseEntity {
   private LocalDateTime expireDate;
 
   @Builder
-  private BookBorrowInfo(Member member, Book book, LocalDateTime borrowDate,
+  private BookBorrowInfo(Member member, Book book, BookBorrowStatus borrowStatus, LocalDateTime borrowDate,
       LocalDateTime expireDate) {
     this.member = member;
     this.book = book;
+    this.borrowStatus = borrowStatus;
     this.borrowDate = borrowDate;
     this.expireDate = expireDate;
   }
