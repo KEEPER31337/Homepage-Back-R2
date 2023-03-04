adf2af2837bdb9510a98985404a02e6580012318
minion.mo <minion.mo@kakaocorp.com>
2023-03-04 13:48:12 +0900

    feat: 도서 추가 비즈니스 로직 제작

--- src/main/java/com/keeper/homepage/domain/library/application/BookManageService.kt
+++ src/main/java/com/keeper/homepage/domain/library/application/BookManageService.kt
@@ -1,13 +1,19 @@
 package com.keeper.homepage.domain.library.application
 
+import com.keeper.homepage.domain.library.dao.BookRepository
+import com.keeper.homepage.domain.library.entity.Book
 import com.keeper.homepage.domain.library.entity.BookDepartment
+import com.keeper.homepage.global.util.thumbnail.ThumbnailUtil
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
 
 @Service
 @Transactional(readOnly = true)
-class BookManageService() {
+class BookManageService(
+    val bookRepository: BookRepository,
+    val thumbnailUtil: ThumbnailUtil
+) {
     @Transactional
     fun addBook(
         title: String,
@@ -15,7 +21,15 @@ class BookManageService() {
         totalQuantity: Long,
         bookDepartment: BookDepartment.BookDepartmentType,
         thumbnail: MultipartFile?
-    ): Long {
-        return 1
-    }
+    ): Long = bookRepository.save(
+        Book(
+            title,
+            author,
+            BookDepartment.getBookDepartmentBy(bookDepartment),
+            totalQuantity,
+            0L,
+            thumbnailUtil.saveThumbnail(thumbnail)
+                .orElse(null)
+        )
+    ).id
 }
