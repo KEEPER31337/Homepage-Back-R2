f96cd8016701726b59b01d35b1dedd4535e33ef1
minion.mo <minion.mo@kakaocorp.com>
2023-03-22 20:30:38 +0900

    refactor: OCP 준수해보기

--- src/main/java/com/keeper/homepage/domain/library/dto/req/BorrowStatusDto.kt
+++ src/main/java/com/keeper/homepage/domain/library/dto/req/BorrowStatusDto.kt
@@ -1,9 +1,35 @@
 package com.keeper.homepage.domain.library.dto.req
 
+import com.keeper.homepage.domain.library.dao.BookBorrowInfoRepository
+import com.keeper.homepage.domain.library.entity.BookBorrowInfo
+import com.keeper.homepage.domain.library.entity.BookBorrowStatus.BookBorrowStatusType.대출대기중
+import com.keeper.homepage.domain.library.entity.BookBorrowStatus.BookBorrowStatusType.반납대기중
+import com.keeper.homepage.domain.library.entity.BookBorrowStatus.getBookBorrowStatusBy
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import java.time.LocalDateTime
+
 enum class BorrowStatusDto(val status: String) {
-    REQUESTS("requests"),
-    WILL_RETURN("willreturn"),
-    OVERDUE("overdue");
+    REQUESTS("requests") {
+        override fun getBorrowInfo(
+            repository: BookBorrowInfoRepository,
+            pageable: Pageable
+        ) = repository.findAllByBorrowStatus(getBookBorrowStatusBy(대출대기중), pageable)!!
+    },
+    WILL_RETURN("willreturn") {
+        override fun getBorrowInfo(
+            repository: BookBorrowInfoRepository,
+            pageable: Pageable
+        ) = repository.findAllByBorrowStatus(getBookBorrowStatusBy(반납대기중), pageable)!!
+    },
+    OVERDUE("overdue") {
+        override fun getBorrowInfo(
+            repository: BookBorrowInfoRepository,
+            pageable: Pageable
+        ) = repository.findAllOverDue(LocalDateTime.now(), pageable)!!
+    };
 
     fun isMatch(status: String) = this.status == status
+
+    abstract fun getBorrowInfo(repository: BookBorrowInfoRepository, pageable: Pageable): Page<BookBorrowInfo>
 }
