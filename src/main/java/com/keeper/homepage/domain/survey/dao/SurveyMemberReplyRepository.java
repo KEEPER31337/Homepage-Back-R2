746b4fca391a50e07db0bb3aeaf73111b7235d0a
shkisme <shkisme0130@gmail.com>
2023-05-05 15:57:26 +0900

    feat: 활동 조사 Repository 개발

---
+++ src/main/java/com/keeper/homepage/domain/survey/dao/SurveyMemberReplyRepository.java
@@ -0,0 +1,8 @@
+package com.keeper.homepage.domain.survey.dao;
+
+import com.keeper.homepage.domain.survey.entity.SurveyMemberReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SurveyMemberReplyRepository extends JpaRepository<SurveyMemberReply, Long> {
+
+}
---
+++ src/main/java/com/keeper/homepage/domain/survey/dao/SurveyReplyRepository.java
@@ -0,0 +1,8 @@
+package com.keeper.homepage.domain.survey.dao;
+
+import com.keeper.homepage.domain.survey.entity.SurveyReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SurveyReplyRepository extends JpaRepository<SurveyReply, Long> {
+
+}
