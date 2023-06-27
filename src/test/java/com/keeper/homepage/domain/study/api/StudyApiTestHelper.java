85c0dfe9bc84ada85d75939c74b8e85f06d6f76a
shkisme <shkisme0130@gmail.com>
2023-06-27 15:35:43 +0900

    test: 스터디 수정, 썸네일 수정, 스터디원 추가 삭제 Controller 테스트 작성

--- src/test/java/com/keeper/homepage/domain/study/api/StudyApiTestHelper.java
+++ src/test/java/com/keeper/homepage/domain/study/api/StudyApiTestHelper.java
@@ -4,12 +4,16 @@ import static com.keeper.homepage.global.config.security.data.JwtType.ACCESS_TOK
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 
 import com.keeper.homepage.IntegrationTest;
+import com.keeper.homepage.domain.study.dto.request.StudyUpdateRequest;
 import jakarta.servlet.http.Cookie;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.MultiValueMap;
 
@@ -50,6 +54,38 @@ public class StudyApiTestHelper extends IntegrationTest {
     return mockMvc.perform(get("/studies")
         .param("year", String.valueOf(year))
         .param("season", String.valueOf(season))
+        .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken)));
+  }
+
+  ResultActions callUpdateStudyApi(String accessToken, long studyId, StudyUpdateRequest request)
+      throws Exception {
+    return mockMvc.perform(put("/studies/{studyId}", studyId)
+        .content(asJsonString(request))
+        .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken))
+        .contentType(MediaType.APPLICATION_JSON));
+  }
+
+  ResultActions callUpdateStudyThumbnailApi(String accessToken, long studyId, MockMultipartFile thumbnail)
+      throws Exception {
+    return mockMvc.perform(RestDocumentationRequestBuilders.multipart("/studies/{studyId}/thumbnail", studyId)
+        .file(thumbnail)
+        .with(request -> {
+          request.setMethod("PATCH");
+          return request;
+        })
+        .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken))
+        .contentType(MediaType.MULTIPART_FORM_DATA));
+  }
+
+  ResultActions callJoinStudyApi(String accessToken, long studyId, long memberId)
+      throws Exception {
+    return mockMvc.perform(post("/studies/{studyId}/members/{memberId}", studyId, memberId)
+        .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken)));
+  }
+
+  ResultActions callLeaveStudyApi(String accessToken, long studyId, long memberId)
+      throws Exception {
+    return mockMvc.perform(delete("/studies/{studyId}/members/{memberId}", studyId, memberId)
         .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken)));
   }
 }
