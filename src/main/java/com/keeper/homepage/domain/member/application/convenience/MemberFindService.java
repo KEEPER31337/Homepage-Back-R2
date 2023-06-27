dae313fa9675d0b629251bdc4d2ec280eeb66d3a
shkisme <shkisme0130@gmail.com>
2023-06-27 15:29:47 +0900

    feat: 스터디 수정, 썸네일 수정 api 개발 + 스터디원 추가, 삭제 api 개발

--- src/main/java/com/keeper/homepage/domain/member/application/convenience/MemberFindService.java
+++ src/main/java/com/keeper/homepage/domain/member/application/convenience/MemberFindService.java
@@ -1,9 +1,11 @@
 package com.keeper.homepage.domain.member.application.convenience;
 
 import static com.keeper.homepage.global.error.ErrorCode.MEMBER_NOT_FOUND;
+import static com.keeper.homepage.global.error.ErrorCode.STUDY_NOT_FOUND;
 
 import com.keeper.homepage.domain.member.dao.MemberRepository;
 import com.keeper.homepage.domain.member.entity.Member;
+import com.keeper.homepage.domain.study.entity.Study;
 import com.keeper.homepage.global.error.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,5 +22,10 @@ public class MemberFindService {
   public Member getVirtualMember() {
     return memberRepository.findById(VIRTUAL_MEMBER_ID)
         .orElseThrow(() -> new BusinessException(VIRTUAL_MEMBER_ID, "memberId", MEMBER_NOT_FOUND));
+  }
+
+  public Member findById(long memberId) {
+    return memberRepository.findByIdAndIdNot(memberId, VIRTUAL_MEMBER_ID)
+        .orElseThrow(() -> new BusinessException(memberId, "memberId", MEMBER_NOT_FOUND));
   }
 }
--- src/main/java/com/keeper/homepage/domain/study/api/StudyController.java
+++ src/main/java/com/keeper/homepage/domain/study/api/StudyController.java
@@ -3,21 +3,27 @@ package com.keeper.homepage.domain.study.api;
 import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.domain.study.application.StudyService;
 import com.keeper.homepage.domain.study.dto.request.StudyCreateRequest;
+import com.keeper.homepage.domain.study.dto.request.StudyUpdateRequest;
 import com.keeper.homepage.domain.study.dto.response.StudyDetailResponse;
 import com.keeper.homepage.domain.study.dto.response.StudyListResponse;
 import com.keeper.homepage.global.config.security.annotation.LoginMember;
 import jakarta.validation.Valid;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -62,5 +68,45 @@ public class StudyController {
     StudyListResponse listResponse = studyService.getStudies(year, season);
     return ResponseEntity.status(HttpStatus.OK)
         .body(listResponse);
+  }
+
+  @PutMapping("/{studyId}")
+  public ResponseEntity<Void> updateStudy(
+      @LoginMember Member member,
+      @PathVariable long studyId,
+      @RequestBody @Valid StudyUpdateRequest request
+  ) {
+    studyService.update(member, studyId, request.toEntity());
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .location(URI.create("/studies/" + studyId))
+        .build();
+  }
+
+  @PatchMapping("/{studyId}/thumbnail")
+  public ResponseEntity<Void> updateStudyThumbnail(
+      @LoginMember Member member,
+      @PathVariable long studyId,
+      @ModelAttribute MultipartFile thumbnail
+  ) {
+    studyService.updateStudyThumbnail(member, studyId, thumbnail);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PostMapping("/{studyId}/members/{memberId}")
+  public ResponseEntity<Void> joinStudy(
+      @PathVariable long studyId,
+      @PathVariable long memberId
+  ) {
+    studyService.joinStudy(studyId, memberId);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @DeleteMapping("/{studyId}/members/{memberId}")
+  public ResponseEntity<Void> leaveStudy(
+      @PathVariable long studyId,
+      @PathVariable long memberId
+  ) {
+    studyService.leaveStudy(studyId, memberId);
+    return ResponseEntity.noContent().build();
   }
 }
