31843b85ebdc33d9ef621e56f61082f6ba1fa35d
shkisme <shkisme0130@gmail.com>
2023-06-29 13:26:41 +0900

    test: 회원 목록 조회(검색) Controller Test 작성

--- src/test/java/com/keeper/homepage/domain/member/api/MemberControllerTest.java
+++ src/test/java/com/keeper/homepage/domain/member/api/MemberControllerTest.java
@@ -1,28 +1,41 @@
 package com.keeper.homepage.domain.member.api;
 
+import static com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType.ROLE_회원;
 import static com.keeper.homepage.global.config.security.data.JwtType.ACCESS_TOKEN;
 import static com.keeper.homepage.global.config.security.data.JwtType.REFRESH_TOKEN;
+import static com.keeper.homepage.global.restdocs.RestDocsHelper.getSecuredValue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
 import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.keeper.homepage.IntegrationTest;
 import com.keeper.homepage.domain.member.dto.request.ChangePasswordRequest;
 import com.keeper.homepage.domain.member.entity.Member;
+import com.keeper.homepage.domain.member.entity.embedded.Generation;
+import com.keeper.homepage.domain.study.api.StudyController;
 import jakarta.servlet.http.Cookie;
+import java.io.IOException;
+import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.snippet.Attributes.Attribute;
 
 class MemberControllerTest extends IntegrationTest {
 
@@ -57,6 +70,47 @@ class MemberControllerTest extends IntegrationTest {
                   fieldWithPath("newPassword").description("새로운 패스워드")),
               responseHeaders(
                   headerWithName(HttpHeaders.LOCATION).description("비밀번호 변경한 리소스의 위치입니다."))));
+    }
+  }
+
+  @Nested
+  @DisplayName("회원 목록 조회")
+  class GetMembers {
+
+    private Member member;
+    private String memberToken;
+
+    @BeforeEach
+    void setUp() throws IOException {
+      member = memberTestHelper.builder().build();
+      memberToken = jwtTokenProvider.createAccessToken(ACCESS_TOKEN, member.getId(), ROLE_회원);
+    }
+
+    @Test
+    @DisplayName("유효한 요청일 경우 회원 목록 조회(검색)는 성공한다.")
+    public void 유효한_요청일_경우_회원_목록_조회는_성공한다() throws Exception {
+      assertThat(member.getGeneration()).isNotNull();
+      String securedValue = getSecuredValue(MemberController.class, "getMembers");
+
+      mockMvc.perform(get("/members")
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), memberToken))
+              .contentType(MediaType.APPLICATION_JSON))
+          .andExpect(status().isOk())
+          .andDo(document("get-members",
+              requestCookies(
+                  cookieWithName(ACCESS_TOKEN.getTokenName())
+                      .description("ACCESS TOKEN %s".formatted(securedValue))
+              ),
+              queryParameters(
+                  parameterWithName("searchName").description("회원 이름 검색어")
+                      .attributes(new Attribute("format", "null : 전체 목록 조회"))
+                      .optional()
+              ),
+              responseFields(
+                  fieldWithPath("[].memberId").description("회원 ID"),
+                  fieldWithPath("[].memberName").description("회원 실명"),
+                  fieldWithPath("[].generation").description("회원 기수")
+              )));
     }
   }
 }
