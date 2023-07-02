36c49f72f679768287f584f3ca92efd91d1de45a
shkisme <shkisme0130@gmail.com>
2023-07-02 18:09:50 +0900

    feat: PostHasFile entity 구현

---
+++ src/main/java/com/keeper/homepage/domain/post/entity/PostHasFile.java
@@ -0,0 +1,41 @@
+package com.keeper.homepage.domain.post.entity;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.keeper.homepage.domain.file.entity.FileEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@EqualsAndHashCode(of = {"post", "file"})
+@IdClass(PostHasFilePK.class)
+@Table(name = "posting_has_file")
+public class PostHasFile {
+
+  @Id
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "posting_id", nullable = false, updatable = false)
+  private Post post;
+
+  @Id
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "file_id", nullable = false, updatable = false)
+  private FileEntity file;
+
+  @Builder
+  private PostHasFile(Post post, FileEntity file) {
+    this.post = post;
+    this.file = file;
+  }
+}
