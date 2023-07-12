e46225edccfac4d865fbc12303387735ade49376
shkisme <shkisme0130@gmail.com>
2023-07-12 18:06:36 +0900

    style: MemberJob.isExecutive() 메서드 사용

--- src/main/java/com/keeper/homepage/domain/member/application/MemberJobService.java
+++ src/main/java/com/keeper/homepage/domain/member/application/MemberJobService.java
@@ -1,17 +1,15 @@
 package com.keeper.homepage.domain.member.application;
 
-import static com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType.ROLE_출제자;
-import static com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType.ROLE_회원;
 import static com.keeper.homepage.global.error.ErrorCode.MEMBER_JOB_IS_NOT_EXECUTIVE;
 import static com.keeper.homepage.global.error.ErrorCode.MEMBER_JOB_NOT_FOUND;
 
 import com.keeper.homepage.domain.member.application.convenience.MemberFindService;
 import com.keeper.homepage.domain.member.dao.role.MemberHasMemberJobRepository;
 import com.keeper.homepage.domain.member.dao.role.MemberJobRepository;
+import com.keeper.homepage.domain.member.dto.response.JobResponse;
 import com.keeper.homepage.domain.member.dto.response.MemberJobResponse;
 import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.domain.member.entity.job.MemberJob;
-import com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType;
 import com.keeper.homepage.global.error.BusinessException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -46,28 +44,26 @@ public class MemberJobService {
   @Transactional
   public void addMemberExecutiveJob(Long memberId, Long jobId) {
     Member member = memberFindService.findById(memberId);
-    MemberJobType memberJobType = memberJobRepository.findById(jobId)
-        .orElseThrow(() -> new BusinessException(jobId, "jobId", MEMBER_JOB_NOT_FOUND))
-        .getType();
+    MemberJob memberJob = memberJobRepository.findById(jobId)
+        .orElseThrow(() -> new BusinessException(jobId, "jobId", MEMBER_JOB_NOT_FOUND));
 
-    checkExecutiveJob(memberJobType);
-    member.assignJob(memberJobType);
+    checkExecutiveJob(memberJob);
+    member.assignJob(memberJob.getType());
   }
 
-  private void checkExecutiveJob(MemberJobType memberJobType) {
-    if (memberJobType.equals(ROLE_회원) || memberJobType.equals(ROLE_출제자)) {
-      throw new BusinessException(memberJobType.toString(), "memberJobType", MEMBER_JOB_IS_NOT_EXECUTIVE);
+  private void checkExecutiveJob(MemberJob memberJob) {
+    if (memberJob.isExecutive()) {
+      throw new BusinessException(memberJob.toString(), "memberJob", MEMBER_JOB_IS_NOT_EXECUTIVE);
     }
   }
 
   @Transactional
   public void deleteMemberExecutiveJob(Long memberId, Long jobId) {
     Member member = memberFindService.findById(memberId);
-    MemberJobType memberJobType = memberJobRepository.findById(jobId)
-        .orElseThrow(() -> new BusinessException(jobId, "jobId", MEMBER_JOB_NOT_FOUND))
-        .getType();
+    MemberJob memberJob = memberJobRepository.findById(jobId)
+        .orElseThrow(() -> new BusinessException(jobId, "jobId", MEMBER_JOB_NOT_FOUND));
 
-    checkExecutiveJob(memberJobType);
-    member.deleteJob(memberJobType);
+    checkExecutiveJob(memberJob);
+    member.deleteJob(memberJob.getType());
   }
 }
