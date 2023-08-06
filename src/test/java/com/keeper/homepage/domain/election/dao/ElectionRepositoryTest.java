765a12c3d678ce2d37311440b6a571e39d81a8bf
이지민 <min102602@naver.com>
2023-08-06 11:35:59 +0900

    refactor : `getVotersInElection` Test 수정

--- src/test/java/com/keeper/homepage/domain/election/dao/ElectionRepositoryTest.java
+++ src/test/java/com/keeper/homepage/domain/election/dao/ElectionRepositoryTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.keeper.homepage.IntegrationTest;
 import com.keeper.homepage.domain.election.entity.Election;
 import com.keeper.homepage.domain.election.entity.ElectionVoter;
-import com.keeper.homepage.domain.member.entity.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -20,19 +19,16 @@ public class ElectionRepositoryTest extends IntegrationTest {
     @Test
     @DisplayName("유권자 목록 수 확인")
     public void getVotersInElection() throws Exception {
-      Member member = memberTestHelper.generate();
-      Election election = electionTestHelper.generate();
       ElectionVoter electionVoter = electionVoterTestHelper.builder()
-          .member(member)
-          .election(election)
           .isVoted(false)
           .build();
 
       em.flush();
       em.clear();
 
-      Election getVoters = electionRepository.getReferenceById(election.getId());
+      Election getVoters = electionRepository.getReferenceById(electionVoter.getElection().getId());
 
+      assertThat(getVoters.getIsAvailable()).isEqualTo(true);
       assertThat(getVoters.getElectionVoters().size()).isEqualTo(1);
     }
   }
