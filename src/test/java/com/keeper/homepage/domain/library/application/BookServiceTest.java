acc12df33bac8822b045672623012be651717aa2
이지민 <min102602@naver.com>
2023-08-09 00:43:27 +0900

    feat : 도서 반납 요청 api test 구현

--- src/test/java/com/keeper/homepage/domain/library/application/BookServiceTest.java
+++ src/test/java/com/keeper/homepage/domain/library/application/BookServiceTest.java
@@ -3,16 +3,20 @@ package com.keeper.homepage.domain.library.application;
 import static com.keeper.homepage.domain.library.application.BookService.MAX_BORROWING_COUNT;
 import static com.keeper.homepage.domain.library.entity.BookBorrowStatus.BookBorrowStatusType.대출대기중;
 import static com.keeper.homepage.domain.library.entity.BookBorrowStatus.BookBorrowStatusType.대출승인;
+import static com.keeper.homepage.domain.library.entity.BookBorrowStatus.BookBorrowStatusType.반납대기중;
 import static com.keeper.homepage.domain.library.entity.BookBorrowStatus.getBookBorrowStatusBy;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.keeper.homepage.IntegrationTest;
 import com.keeper.homepage.domain.library.dto.resp.BookResponse;
 import com.keeper.homepage.domain.library.entity.Book;
+import com.keeper.homepage.domain.library.entity.BookBorrowInfo;
 import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.global.error.BusinessException;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -47,7 +51,8 @@ public class BookServiceTest extends IntegrationTest {
     @Test
     @DisplayName("현재수량이 0이라면 도서 대여 불가능 응답을 해야 한다.")
     public void 현재수량이_0이라면_도서_대여_불가능_응답을_해야_한다() throws Exception {
-      bookBorrowInfoTestHelper.builder().book(book).borrowStatus(getBookBorrowStatusBy(대출승인)).build();
+      bookBorrowInfoTestHelper.builder().book(book).borrowStatus(getBookBorrowStatusBy(대출승인))
+          .build();
 
       Page<BookResponse> books = bookService.getBooks(member, null, null, PageRequest.of(0, 5));
 
@@ -142,6 +147,80 @@ public class BookServiceTest extends IntegrationTest {
       member = memberRepository.findById(member.getId()).get();
 
       assertThrows(BusinessException.class, () -> bookService.requestBorrow(member, book.getId()));
+    }
+  }
+
+  @Nested
+  @DisplayName("도서 반납 요청")
+  class requestReturn {
+
+    private Member member;
+    private Book book;
+
+    @BeforeEach
+    void setUp() {
+      member = memberTestHelper.generate();
+      book = bookTestHelper.generate();
+    }
+
+    @Test
+    @DisplayName("도서 반납 요청 시 requestDateTime이 갱신되어야 한다.")
+    public void 도서_반납_요청_시_requestDateTime이_갱신되어야_한다() throws Exception {
+      BookBorrowInfo bookBorrowInfo = bookBorrowInfoTestHelper.builder()
+          .member(member)
+          .book(book)
+          .borrowStatus(getBookBorrowStatusBy(대출승인))
+          .build();
+
+      long borrowId = bookBorrowInfo.getId();
+
+      LocalDateTime now = LocalDateTime.now();
+      bookService.requestReturn(member, borrowId);
+      BookBorrowInfo updateTime = bookBorrowInfoRepository.findById(borrowId).orElse(null);
+
+      assertThat(updateTime).isNotNull();
+      assertNotEquals(now, updateTime.getLastRequestDate());
+    }
+
+    @Test
+    @DisplayName("도서 반납 요청 시 requestDateTime이 갱신되어야 한다.")
+    public void 도서_반납_요청_시_도서_상태가_갱신되어야_한다() throws Exception {
+      BookBorrowInfo bookBorrowInfo = bookBorrowInfoTestHelper.builder()
+          .member(member)
+          .book(book)
+          .borrowStatus(getBookBorrowStatusBy(대출승인))
+          .build();
+
+      long borrowId = bookBorrowInfo.getId();
+
+      bookService.requestReturn(member, borrowId);
+      BookBorrowInfo borrowInfo = bookBorrowInfoRepository.findById(borrowId).orElse(null);
+
+      assertThat(borrowInfo).isNotNull();
+      assertThat(borrowInfo.getBorrowStatus().getType()).isEqualTo(반납대기중);
+    }
+
+    @Test
+    @DisplayName("대출 내역이 없다면 도서 반납 요청이 실패해야 한다.")
+    public void 대출_내역이_없다면_도서_반납_요청이_실패해야_한다() throws Exception {
+      long borrowId = 5;
+
+      assertThrows(BusinessException.class, () -> bookService.requestReturn(member, borrowId));
+    }
+
+    @Test
+    @DisplayName("본인이 아니라면 도서 반납 요청이 실패해야 한다.")
+    public void 본인이_아니라면_도서_반납_요청이_실패해아_한다() throws Exception {
+      Member otherMember = memberTestHelper.generate();
+      BookBorrowInfo bookBorrowInfo = bookBorrowInfoTestHelper.builder()
+          .member(otherMember)
+          .book(book)
+          .borrowStatus(getBookBorrowStatusBy(대출승인))
+          .build();
+
+      long borrowId = bookBorrowInfo.getId();
+
+      assertThrows(BusinessException.class, () -> bookService.requestReturn(member, borrowId));
     }
   }
 }
