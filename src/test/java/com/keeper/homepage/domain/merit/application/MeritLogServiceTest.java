5e2dcb3b86dc5b5915449d254209faf54bb39d91
02ggang9 <02ggang9@gmail.com>
2023-08-09 21:05:26 +0900

    test: 상벌점 목록 조회 테스트 코드 작성

--- src/test/java/com/keeper/homepage/domain/merit/application/MeritLogServiceTest.java
+++ src/test/java/com/keeper/homepage/domain/merit/application/MeritLogServiceTest.java
@@ -1,5 +1,6 @@
 package com.keeper.homepage.domain.merit.application;
 
+import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,10 +8,14 @@ import com.keeper.homepage.IntegrationTest;
 import com.keeper.homepage.domain.merit.entity.MeritLog;
 import com.keeper.homepage.domain.merit.entity.MeritType;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 class MeritLogServiceTest extends IntegrationTest {
 
@@ -36,6 +41,22 @@ class MeritLogServiceTest extends IntegrationTest {
       assertThat(findMeritLog.getAwarder().getId()).isEqualTo(awarderId);
       assertThat(findMeritLog.getGiver().getId()).isEqualTo(giverId);
       assertThat(findMeritLog.getMeritType().getId()).isEqualTo(meritType.getId());
+    }
+
+    @Test
+    @DisplayName("상벌점 페이지를 조회할 수 있어야 한다.")
+    void 상벌점_페이지를_조회할_수_있어야_한다() {
+      Long meritLogId1 = meritLogTestHelper.generate().getId();
+      Long meritLogId2 = meritLogTestHelper.generate().getId();
+
+      em.flush();
+      em.clear();
+
+      Page<MeritLog> meritLogs = meritLogService.findAll(PageRequest.of(0, 10));
+
+      assertThat(meritLogs.stream().map(MeritLog::getId).collect(toList())).contains(meritLogId1,
+          meritLogId2);
+
     }
   }
 }
