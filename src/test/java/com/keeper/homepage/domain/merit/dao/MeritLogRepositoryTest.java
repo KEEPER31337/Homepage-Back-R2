14d92fb230ac1e86b1fa9f4b71f916baa48f0715
02ggang9 <02ggang9@gmail.com>
2023-08-10 14:08:03 +0900

    test: MeritLogRepository의 수여자 ID로 조회하는 테스트 코드 작성

--- src/test/java/com/keeper/homepage/domain/merit/dao/MeritLogRepositoryTest.java
+++ src/test/java/com/keeper/homepage/domain/merit/dao/MeritLogRepositoryTest.java
@@ -6,13 +6,17 @@ import com.keeper.homepage.domain.member.dao.MemberRepository;
 import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.domain.merit.entity.MeritLog;
 import com.keeper.homepage.domain.merit.entity.MeritType;
+import java.util.stream.Collectors;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
+import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,8 +24,7 @@ public class MeritLogRepositoryTest extends IntegrationTest {
 
   @Nested
   @DisplayName("상벌점 기능 테스트")
-  @Component
-  class MeritTest {
+  class MeritLogSaveTest {
 
     @Test
     @DisplayName("DB에 상벌점 로그 등록을 성공해야 한다.")
@@ -36,8 +39,32 @@ public class MeritLogRepositoryTest extends IntegrationTest {
       assertThat(meritLog.getId()).isEqualTo(findMeritLog.getId());
       assertThat(meritLog.getGiver()).isEqualTo(findMeritLog.getGiver());
       assertThat(meritLog.getAwarder()).isEqualTo(findMeritLog.getAwarder());
-      assertThat(meritLog.getTime()).isBefore(findMeritLog.getTime());
+      assertThat(meritLog.getTime()).isAfter(findMeritLog.getTime());
       assertThat(meritLog.getMeritType().getId()).isEqualTo(findMeritLog.getMeritType().getId());
+
+    }
+  }
+
+  @Nested
+  @DisplayName("상벌점 로그 조회 테스트")
+  class MeritLogSearchTest {
+
+    @Test
+    @DisplayName("상벌점 로그를 수여자 ID로 조회할 수 있어야 한다.")
+    void 상벌점_로그를_수여자_ID로_조회할_수_있어야_한다() {
+      Member giver = memberTestHelper.generate();
+      Long meritLogIdByGiver = meritLogTestHelper.builder().giver(giver).build().getId();
+      meritLogTestHelper.generate();
+      meritLogTestHelper.generate();
+
+      em.flush();
+      em.clear();
+
+      Page<MeritLog> pages = meritLogRepository.findByGiver_Id(PageRequest.of(0, 10),
+          giver.getId());
+
+      assertThat(pages.stream().map(MeritLog::getId).collect(toList()))
+          .contains(meritLogIdByGiver);
 
     }
   }
