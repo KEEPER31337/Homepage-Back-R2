c7dd1302b91914f4ccfdebf03b96bb9bc2e6003a
02ggang9 <02ggang9@gmail.com>
2023-08-10 13:30:34 +0900

    refactor: MeritLogServiceTest 중복 코드 제거 및 변수명 변경

--- src/test/java/com/keeper/homepage/domain/merit/application/MeritLogServiceTest.java
+++ src/test/java/com/keeper/homepage/domain/merit/application/MeritLogServiceTest.java
@@ -10,6 +10,7 @@ import com.keeper.homepage.domain.merit.entity.MeritType;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,18 @@ import org.springframework.data.domain.Pageable;
 
 class MeritLogServiceTest extends IntegrationTest {
 
+  private Member giver, awarder;
+  private MeritType meritType;
+  private long giverId, awarderId;
+  private long meritLogId, otherMeritLogId;
+
+
+  @BeforeEach
+  void setUp() {
+    giver = memberTestHelper.generate();
+    awarder = memberTestHelper.generate();
+  }
+
   @Nested
   @DisplayName("상벌점 부여 테스트")
   class MeritLogTest {
@@ -26,9 +39,9 @@ class MeritLogServiceTest extends IntegrationTest {
     @Test
     @DisplayName("상벌점 부여를 성공해야 한다.")
     void 상벌점_부여를_성공해야_한다() {
-      Long awarderId = memberTestHelper.generate().getId();
-      Long giverId = memberTestHelper.generate().getId();
-      MeritType meritType = meritTypeHelper.generate();
+      awarderId = awarder.getId();
+      giverId = giver.getId();
+      meritType = meritTypeHelper.generate();
 
       Long meritLogId = meritLogService.recordMerit(awarderId, giverId, "무단 결석");
 
@@ -46,26 +59,25 @@ class MeritLogServiceTest extends IntegrationTest {
     @Test
     @DisplayName("상벌점 페이지를 조회할 수 있어야 한다.")
     void 상벌점_페이지를_조회할_수_있어야_한다() {
-      Long meritLogId1 = meritLogTestHelper.generate().getId();
-      Long meritLogId2 = meritLogTestHelper.generate().getId();
+      meritLogId = meritLogTestHelper.generate().getId();
+      otherMeritLogId = meritLogTestHelper.generate().getId();
 
       em.flush();
       em.clear();
 
       Page<MeritLog> meritLogs = meritLogService.findAll(PageRequest.of(0, 10));
 
-      assertThat(meritLogs.stream().map(MeritLog::getId).collect(toList())).contains(meritLogId1,
-          meritLogId2);
+      assertThat(meritLogs.stream()
+          .map(MeritLog::getId)
+          .collect(toList())).contains(meritLogId, otherMeritLogId);
 
     }
 
     @Test
     @DisplayName("상벌점 페이지를 ID 값으로 조회할 수 있어야 한다.")
     void 상벌점_페이즈를_ID_값으로_조회할_수_있어야_한다() {
-      Member giver = memberTestHelper.generate();
-      Long meritLogId1 = meritLogTestHelper.builder().giver(giver).build().getId();
-      Long meritLogId2 = meritLogTestHelper.builder().giver(giver).build().getId();
-      Long meritLogId3 = meritLogTestHelper.builder().giver(giver).build().getId();
+      meritLogId = meritLogTestHelper.builder().giver(giver).build().getId();
+      otherMeritLogId = meritLogTestHelper.builder().giver(giver).build().getId();
 
       em.flush();
       em.clear();
@@ -73,8 +85,10 @@ class MeritLogServiceTest extends IntegrationTest {
       Page<MeritLog> meritLogPage = meritLogService.findByGiver_Id(PageRequest.of(0, 10),
           giver.getId());
 
-      assertThat(meritLogPage.stream().map(MeritLog::getId).collect(toList()))
-          .contains(meritLogId1, meritLogId2, meritLogId3);
+      assertThat(meritLogPage.stream()
+          .map(MeritLog::getId)
+          .collect(toList()))
+          .contains(meritLogId, otherMeritLogId);
     }
   }
 }
