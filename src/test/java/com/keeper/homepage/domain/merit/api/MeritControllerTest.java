d0cce73895aaf9fbadff74a97476d4ff694edf5c
02ggang9 <02ggang9@gmail.com>
2023-08-11 22:52:45 +0900

    test: 상벌점 페이지 접근 권한 테스트 코드 작성

--- src/test/java/com/keeper/homepage/domain/merit/api/MeritControllerTest.java
+++ src/test/java/com/keeper/homepage/domain/merit/api/MeritControllerTest.java
@@ -1,5 +1,6 @@
 package com.keeper.homepage.domain.merit.api;
 
+import static com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType.*;
 import static com.keeper.homepage.global.config.security.data.JwtType.ACCESS_TOKEN;
 import static com.keeper.homepage.global.restdocs.RestDocsHelper.getSecuredValue;
 import static com.keeper.homepage.global.restdocs.RestDocsHelper.pageHelper;
@@ -21,6 +22,7 @@ import static org.springframework.restdocs.request.RequestDocumentation.requestP
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.keeper.homepage.IntegrationTest;
@@ -47,16 +49,18 @@ public class MeritControllerTest extends MeritApiTestHelper {
 
 
   private MeritType meritType;
-  private Member member, other;
-  private String accessToken;
+  private Member member, admin;
+  private String userAccessToken, adminAccessToken;
 
   @BeforeEach
   void setUp() throws IOException {
     meritType = meritTypeHelper.generate();
     member = memberTestHelper.generate();
-    other = memberTestHelper.generate();
-    accessToken = jwtTokenProvider.createAccessToken(ACCESS_TOKEN, member.getId(),
-        MemberJobType.ROLE_회원);
+    admin = memberTestHelper.generate();
+    userAccessToken = jwtTokenProvider.createAccessToken(ACCESS_TOKEN, member.getId(),
+        ROLE_회원);
+    adminAccessToken = jwtTokenProvider.createAccessToken(ACCESS_TOKEN, member.getId(),
+        ROLE_회장, ROLE_부회장, ROLE_서기, ROLE_회원);
   }
 
   @Nested
@@ -68,7 +72,7 @@ public class MeritControllerTest extends MeritApiTestHelper {
     void 상벌점_조회는_성공해야_한다() throws Exception {
       String securedValue = getSecuredValue(MeritController.class, "searchMeritType");
       mockMvc.perform(get("/merits/types")
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken)))
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), adminAccessToken)))
           .andExpect(status().isOk())
           .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
           .andDo(document("search-meritType",
@@ -82,6 +86,15 @@ public class MeritControllerTest extends MeritApiTestHelper {
     }
 
     @Test
+    @DisplayName("일반 회원은 조회할 수 없다.")
+    void 일반_회원은_조회할_수_없다() throws Exception {
+      mockMvc.perform(get("/merits/types")
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), userAccessToken)))
+          .andExpect(status().isForbidden())
+          .andExpect(jsonPath("$.message").exists());
+    }
+
+    @Test
     @DisplayName("상벌점 타입 생성을 성공해야 한다.")
     void 상벌점_타입_생성을_성공해야_한다() throws Exception {
       String securedValue = getSecuredValue(MeritController.class, "registerMeritType");
@@ -91,7 +104,7 @@ public class MeritControllerTest extends MeritApiTestHelper {
           .build();
 
       mockMvc.perform(post("/merits/types")
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken))
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), adminAccessToken))
               .content(asJsonString(request))
               .contentType(MediaType.APPLICATION_JSON))
           .andExpect(status().isCreated())
@@ -107,6 +120,22 @@ public class MeritControllerTest extends MeritApiTestHelper {
     }
 
     @Test
+    @DisplayName("일반 회원은 상벌점 타입을 생성할 수 없다.")
+    void 일반_회원은_상벌점_타입을_생성할_수_없다() throws Exception {
+      AddMeritTypeRequest request = AddMeritTypeRequest.builder()
+          .score(3)
+          .reason("우수기술문서 작성")
+          .build();
+
+      mockMvc.perform(post("/merits/types")
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), userAccessToken))
+              .content(asJsonString(request))
+              .contentType(MediaType.APPLICATION_JSON))
+          .andExpect(status().isForbidden())
+          .andExpect(jsonPath("$.message").exists());
+    }
+
+    @Test
     @DisplayName("상벌점 타입 수정을 성공해야 한다.")
     void 상벌점_부여_로그_수정을_성공해야_한다() throws Exception {
       String securedValue = getSecuredValue(MeritController.class, "updateMeritType");
@@ -116,7 +145,7 @@ public class MeritControllerTest extends MeritApiTestHelper {
           .build();
 
       mockMvc.perform(put("/merits/types/{meritTypeId}", meritType.getId())
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken))
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), adminAccessToken))
               .contentType(MediaType.APPLICATION_JSON)
               .content(asJsonString(request)))
           .andExpect(status().isCreated())
@@ -129,6 +158,22 @@ public class MeritControllerTest extends MeritApiTestHelper {
                   fieldWithPath("score").description("수정할 점수"),
                   fieldWithPath("reason").description("수정할 사유")
               )));
+    }
+
+    @Test
+    @DisplayName("일반회원은 상벌점 타입 수정을 성공할 수 없다.")
+    void 일반회원은_상벌점_타입_수정을_성공할_수_없다() throws Exception {
+      UpdateMeritTypeRequest request = UpdateMeritTypeRequest.builder()
+          .score(5)
+          .reason("거짓 스터디")
+          .build();
+
+      mockMvc.perform(put("/merits/types/{meritTypeId}", meritType.getId())
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), userAccessToken))
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(asJsonString(request)))
+          .andExpect(status().isForbidden())
+          .andExpect(jsonPath("$.message").exists());
     }
   }
 
@@ -150,7 +195,7 @@ public class MeritControllerTest extends MeritApiTestHelper {
 
       mockMvc.perform(
               get("/merits/members/{memberId}", member.getId())
-                  .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken)))
+                  .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), adminAccessToken)))
           .andExpect(status().isOk())
           .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
           .andDo(document("search-member-meritLog",
@@ -167,12 +212,26 @@ public class MeritControllerTest extends MeritApiTestHelper {
     }
 
     @Test
+    @DisplayName("일반회원은 회원별 상벌점 목록 조회를 할 수 없다.")
+    void 일반회원은_회원별_상벌점_목록_조회를_할_수_없다() throws Exception {
+      meritLogTestHelper.builder().giver(member).build();
+
+      mockMvc.perform(
+              get("/merits/members/{memberId}", member.getId())
+                  .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), userAccessToken)))
+          .andExpect(status().isForbidden())
+          .andExpect(jsonPath("$.message").exists());
+
+
+    }
+
+    @Test
     @DisplayName("상벌점 목록 조회를 성공해야 한다.")
     void 상벌점_목록_조회를_성공해야_한다() throws Exception {
       String securedValue = getSecuredValue(MeritController.class, "searchMeritLogList");
 
       mockMvc.perform(get("/merits")
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken)))
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), adminAccessToken)))
           .andExpect(status().isOk())
           .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
           .andDo(document("search-meritLog",
@@ -186,8 +245,19 @@ public class MeritControllerTest extends MeritApiTestHelper {
     }
 
     @Test
+    @DisplayName("일반회원은 상벌점 목록 조회를 할 수 없다.")
+    void 일반회원은_상벌점_목록_조회를_할_수_없다() throws Exception {
+      mockMvc.perform(get("/merits")
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), userAccessToken)))
+          .andExpect(status().isForbidden())
+          .andExpect(jsonPath("$.message").exists());
+    }
+
+
+    @Test
     @DisplayName("상벌점 부여 로그 생성을 성공해야 한다.")
     void 상벌점_부여_로그_생성을_성공해야_한다() throws Exception {
+
       String securedValue = getSecuredValue(MeritController.class, "registerMerit");
       GiveMeritPointRequest request = GiveMeritPointRequest.builder()
           .awarderId(member.getId())
@@ -195,7 +265,7 @@ public class MeritControllerTest extends MeritApiTestHelper {
           .build();
 
       mockMvc.perform(multipart("/merits")
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken))
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), adminAccessToken))
               .contentType(MediaType.APPLICATION_JSON)
               .content(asJsonString(request)))
           .andExpect(status().isCreated())
@@ -208,6 +278,22 @@ public class MeritControllerTest extends MeritApiTestHelper {
                   fieldWithPath("awarderId").description("수여자의 ID"),
                   fieldWithPath("meritTypeId").description("상벌점 타입의 ID")
               )));
+    }
+
+    @Test
+    @DisplayName("일반회원은 상벌점 부여 로그를 생성할 수 없다.")
+    void 일반회원은_상벌점_부여_로그를_생성할_수_없다() throws Exception {
+      GiveMeritPointRequest request = GiveMeritPointRequest.builder()
+          .awarderId(member.getId())
+          .meritTypeId(meritType.getId())
+          .build();
+
+      mockMvc.perform(multipart("/merits")
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), userAccessToken))
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(asJsonString(request)))
+          .andExpect(status().isForbidden())
+          .andExpect(jsonPath("$.message").exists());
     }
   }
 }
