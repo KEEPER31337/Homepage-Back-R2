9b22a66aa1f70331a80cc274010b9e7030c4485e
02ggang9 <02ggang9@gmail.com>
2023-08-11 19:34:27 +0900

    refactor: MeritController 중복 코드 제거 및 위치 변경

--- src/test/java/com/keeper/homepage/domain/merit/api/MeritControllerTest.java
+++ src/test/java/com/keeper/homepage/domain/merit/api/MeritControllerTest.java
@@ -48,61 +48,109 @@ public class MeritControllerTest extends MeritApiTestHelper {
 
   private MeritType meritType;
   private Member member, other;
-  private String memberToken, otherToken;
+  private String accessToken;
 
   @BeforeEach
   void setUp() throws IOException {
-    meritType = meritTypeHelper.builder().merit(3).detail("우수기술문서작성").build();
+    meritType = meritTypeHelper.generate();
     member = memberTestHelper.generate();
     other = memberTestHelper.generate();
-    memberToken = jwtTokenProvider.createAccessToken(ACCESS_TOKEN, member.getId(),
+    accessToken = jwtTokenProvider.createAccessToken(ACCESS_TOKEN, member.getId(),
         MemberJobType.ROLE_회원);
   }
 
-
   @Nested
-  @DisplayName("상벌점 테스트")
-  @Component
-  class ControllerTest {
-
+  @DisplayName("상벌점 타입 테스트")
+  class MeritTypeTest {
 
     @Test
-    @DisplayName("상벌점 목록 조회를 성공해야 한다.")
-    void 상벌점_목록_조회를_성공해야_한다() throws Exception {
-      String securedValue = getSecuredValue(MeritController.class, "searchMeritLogList");
-
-      meritLogTestHelper.generate();
-      meritLogTestHelper.generate();
-      meritLogTestHelper.generate();
-
-      ResultActions perform = mockMvc.perform(get("/merits")
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), memberToken)))
+    @DisplayName("상벌점 타입 조회를 성공해야 한다.")
+    void 상벌점_조회는_성공해야_한다() throws Exception {
+      String securedValue = getSecuredValue(MeritController.class, "searchMeritType");
+      mockMvc.perform(get("/merits/types")
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken)))
           .andExpect(status().isOk())
           .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-          .andDo(document("search-meritLog",
+          .andDo(document("search-meritType",
               requestCookies(
                   cookieWithName(ACCESS_TOKEN.getTokenName())
                       .description("ACCESS TOKEN %s".formatted(securedValue))
               ),
               responseFields(
-                  pageHelper(getMeritLogResponse())
+                  pageHelper(getMeritTypeResponse())
               )));
+    }
+
+    @Test
+    @DisplayName("상벌점 타입 생성을 성공해야 한다.")
+    void 상벌점_타입_생성을_성공해야_한다() throws Exception {
+      String securedValue = getSecuredValue(MeritController.class, "registerMeritType");
+      AddMeritTypeRequest request = AddMeritTypeRequest.builder()
+          .score(3)
+          .reason("우수기술문서 작성")
+          .build();
+
+      mockMvc.perform(post("/merits/types")
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken))
+              .content(asJsonString(request))
+              .contentType(MediaType.APPLICATION_JSON))
+          .andExpect(status().isCreated())
+          .andDo(document("create-meritType",
+              requestCookies(
+                  cookieWithName(ACCESS_TOKEN.getTokenName())
+                      .description("ACCESS TOKEN %s".formatted(securedValue))
+              ),
+              requestFields(
+                  fieldWithPath("score").description("상벌점 점수를 입력해주세요."),
+                  fieldWithPath("reason").description("상벌점 사유를 입력해주세요.")
+              )));
+    }
+
+    @Test
+    @DisplayName("상벌점 타입 수정을 성공해야 한다.")
+    void 상벌점_부여_로그_수정을_성공해야_한다() throws Exception {
+      String securedValue = getSecuredValue(MeritController.class, "updateMeritType");
+      UpdateMeritTypeRequest request = UpdateMeritTypeRequest.builder()
+          .score(5)
+          .reason("거짓 스터디")
+          .build();
+
+      mockMvc.perform(put("/merits/types/{meritTypeId}", meritType.getId())
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken))
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(asJsonString(request)))
+          .andExpect(status().isCreated())
+          .andDo(document("update-meritType",
+              requestCookies(
+                  cookieWithName(ACCESS_TOKEN.getTokenName())
+                      .description("ACCESS TOKEN %s".formatted(securedValue))
+              ),
+              requestFields(
+                  fieldWithPath("score").description("수정할 점수"),
+                  fieldWithPath("reason").description("수정할 사유")
+              )));
+    }
+  }
+
+  @Nested
+  @DisplayName("상벌점 로그 테스트")
+  class MeritLogTest {
+
+    @BeforeEach
+    void setUp() throws IOException {
+      meritLogTestHelper.generate();
+      meritLogTestHelper.generate();
     }
 
     @Test
     @DisplayName("회원별 상벌점 목록 조회를 성공해야 한다.")
     void 회원별_상벌점_목록_조회를_성공해야_한다() throws Exception {
       String securedValue = getSecuredValue(MeritController.class, "findMeritLogByMemberId");
+      meritLogTestHelper.builder().giver(member).build();
 
-      meritLogTestHelper.builder().giver(member).build();
-      meritLogTestHelper.builder().giver(member).build();
-      meritLogTestHelper.builder().giver(member).build();
-      meritLogTestHelper.builder().giver(other).build();
-      meritLogTestHelper.builder().giver(other).build();
-
-      ResultActions resultActions = mockMvc.perform(
+      mockMvc.perform(
               get("/merits/members/{memberId}", member.getId())
-                  .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), memberToken)))
+                  .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken)))
           .andExpect(status().isOk())
           .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
           .andDo(document("search-member-meritLog",
@@ -119,77 +167,35 @@ public class MeritControllerTest extends MeritApiTestHelper {
     }
 
     @Test
-    @DisplayName("상벌점 타입 조회를 성공해야 한다.")
-    void 상벌점_조회는_성공해야_한다() throws Exception {
-      String securedValue = getSecuredValue(MeritController.class, "searchMeritType");
+    @DisplayName("상벌점 목록 조회를 성공해야 한다.")
+    void 상벌점_목록_조회를_성공해야_한다() throws Exception {
+      String securedValue = getSecuredValue(MeritController.class, "searchMeritLogList");
 
-      ResultActions perform = mockMvc.perform(get("/merits/types")
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), memberToken)))
+      mockMvc.perform(get("/merits")
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken)))
           .andExpect(status().isOk())
           .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-          .andDo(document("search-meritType",
+          .andDo(document("search-meritLog",
               requestCookies(
                   cookieWithName(ACCESS_TOKEN.getTokenName())
                       .description("ACCESS TOKEN %s".formatted(securedValue))
               ),
               responseFields(
-                  pageHelper(getMeritTypeResponse())
+                  pageHelper(getMeritLogResponse())
               )));
     }
-  }
-
-  @Nested
-  @DisplayName("상벌점 타입 생성 테스트")
-  class MeritTypeTest {
-
-    @Test
-    @DisplayName("상벌점 타입 생성을 성공해야 한다.")
-    void 상벌점_타입_생성을_성공해야_한다() throws Exception {
-      String securedValue = getSecuredValue(MeritController.class, "registerMeritType");
-
-      AddMeritTypeRequest request = AddMeritTypeRequest.builder()
-          .score(3)
-          .reason("우수기술문서 작성")
-          .build();
-
-      mockMvc.perform(post("/merits/types")
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), memberToken))
-              .content(asJsonString(request))
-              .contentType(MediaType.APPLICATION_JSON))
-          .andExpect(status().isCreated())
-          .andDo(document("create-meritType",
-              requestCookies(
-                  cookieWithName(ACCESS_TOKEN.getTokenName())
-                      .description("ACCESS TOKEN %s".formatted(securedValue))
-              ),
-              requestFields(
-                  fieldWithPath("score").description("상벌점 점수를 입력해주세요."),
-                  fieldWithPath("reason").description("상벌점 사유를 입력해주세요.")
-              )));
-    }
-
-  }
-
-  @Nested
-  @DisplayName("상벌점 부여 로그 생성 테스트")
-  class MeritLogTest {
 
     @Test
     @DisplayName("상벌점 부여 로그 생성을 성공해야 한다.")
     void 상벌점_부여_로그_생성을_성공해야_한다() throws Exception {
       String securedValue = getSecuredValue(MeritController.class, "registerMerit");
-
       GiveMeritPointRequest request = GiveMeritPointRequest.builder()
           .awarderId(member.getId())
           .meritTypeId(meritType.getId())
           .build();
 
-      MockPart mockPart = new MockPart("request", asJsonString(request).getBytes(
-          StandardCharsets.UTF_8));
-      mockPart.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-
       mockMvc.perform(multipart("/merits")
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), memberToken))
+              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), accessToken))
               .contentType(MediaType.APPLICATION_JSON)
               .content(asJsonString(request)))
           .andExpect(status().isCreated())
@@ -201,37 +207,6 @@ public class MeritControllerTest extends MeritApiTestHelper {
               requestFields(
                   fieldWithPath("awarderId").description("수여자의 ID"),
                   fieldWithPath("meritTypeId").description("상벌점 타입의 ID")
-              )));
-    }
-  }
-
-  @Nested
-  @DisplayName("상벌점 부여 로그 수정 테스트")
-  class MeritLogUpdateTest {
-
-    @Test
-    @DisplayName("상벌점 부여 로그 수정을 성공해야 한다.")
-    void 상벌점_부여_로그_수정을_성공해야_한다() throws Exception {
-      String securedValue = getSecuredValue(MeritController.class, "updateMeritType");
-
-      UpdateMeritTypeRequest request = UpdateMeritTypeRequest.builder()
-          .score(5)
-          .reason("거짓 스터디")
-          .build();
-
-      mockMvc.perform(put("/merits/types/{meritTypeId}", meritType.getId())
-              .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), memberToken))
-              .contentType(MediaType.APPLICATION_JSON)
-              .content(asJsonString(request)))
-          .andExpect(status().isCreated())
-          .andDo(document("update-meritType",
-              requestCookies(
-                  cookieWithName(ACCESS_TOKEN.getTokenName())
-                      .description("ACCESS TOKEN %s".formatted(securedValue))
-              ),
-              requestFields(
-                  fieldWithPath("score").description("수정할 점수"),
-                  fieldWithPath("reason").description("수정할 사유")
               )));
     }
   }
