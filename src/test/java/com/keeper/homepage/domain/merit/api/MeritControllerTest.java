f980cf7c77ee31bcc5d8854d87aa5d10c87c73e0
02ggang9 <02ggang9@gmail.com>
2023-08-11 15:48:08 +0900

    test: 상벌점 목록 조회 API 문서 정리 코드 작성

--- src/test/java/com/keeper/homepage/domain/merit/api/MeritControllerTest.java
+++ src/test/java/com/keeper/homepage/domain/merit/api/MeritControllerTest.java
@@ -1,38 +1,38 @@
 package com.keeper.homepage.domain.merit.api;
 
 import static com.keeper.homepage.global.config.security.data.JwtType.ACCESS_TOKEN;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static com.keeper.homepage.global.restdocs.RestDocsHelper.getSecuredValue;
+import static com.keeper.homepage.global.restdocs.RestDocsHelper.pageHelper;
+import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
+import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.keeper.homepage.IntegrationTest;
 import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType;
-import com.keeper.homepage.domain.merit.application.MeritTypeService;
-import com.keeper.homepage.domain.merit.dao.MeritTypeRepository;
 import com.keeper.homepage.domain.merit.dto.request.UpdateMeritTypeRequest;
-import com.keeper.homepage.domain.merit.entity.MeritLog;
 import com.keeper.homepage.domain.merit.entity.MeritType;
-import com.keeper.homepage.global.config.security.data.JwtType;
 import io.jsonwebtoken.io.IOException;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
-public class MeritControllerTest extends IntegrationTest {
+public class MeritControllerTest extends MeritApiTestHelper{
 
 
   private MeritType meritType;
@@ -42,7 +42,6 @@ public class MeritControllerTest extends IntegrationTest {
   @BeforeEach
   void setUp() throws IOException {
     meritType = meritTypeHelper.builder().merit(3).detail("우수기술문서작성").build();
-    meritType = meritTypeHelper.builder().merit(-3).detail("무단 결석").build();
     member = memberTestHelper.generate();
     other = memberTestHelper.generate();
     memberToken = jwtTokenProvider.createAccessToken(ACCESS_TOKEN, member.getId(),
@@ -59,6 +58,8 @@ public class MeritControllerTest extends IntegrationTest {
     @Test
     @DisplayName("상벌점 목록 조회를 성공해야 한다.")
     void 상벌점_목록_조회를_성공해야_한다() throws Exception {
+      String securedValue = getSecuredValue(MeritController.class, "searchMeritLogList");
+
       meritLogTestHelper.generate();
       meritLogTestHelper.generate();
       meritLogTestHelper.generate();
@@ -67,23 +68,42 @@ public class MeritControllerTest extends IntegrationTest {
               .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), memberToken)))
           .andExpect(status().isOk())
           .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-          .andDo(print());
+          .andDo(document("search-meritLog",
+              requestCookies(
+                  cookieWithName(ACCESS_TOKEN.getTokenName())
+                      .description("ACCESS TOKEN %s".formatted(securedValue))
+              ),
+              responseFields(
+                  pageHelper(getMeritLogResponse())
+              )));
     }
 
     @Test
     @DisplayName("회원별 상벌점 목록 조회를 성공해야 한다.")
     void 회원별_상벌점_목록_조회를_성공해야_한다() throws Exception {
+      String securedValue = getSecuredValue(MeritController.class, "findMeritLogByMemberId");
+
       meritLogTestHelper.builder().giver(member).build();
       meritLogTestHelper.builder().giver(member).build();
       meritLogTestHelper.builder().giver(member).build();
       meritLogTestHelper.builder().giver(other).build();
       meritLogTestHelper.builder().giver(other).build();
 
-      ResultActions resultActions = mockMvc.perform(get("/merits/members/" + member.getId())
+      ResultActions resultActions = mockMvc.perform(get("/merits/members/{memberId}", member.getId())
               .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), memberToken)))
           .andExpect(status().isOk())
           .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-          .andDo(print());
+          .andDo(document("search-member-meritLog",
+              requestCookies(
+                  cookieWithName(ACCESS_TOKEN.getTokenName())
+                      .description("ACCESS TOKEN %s".formatted(securedValue))
+              ),
+              pathParameters(
+                  parameterWithName("memberId").description("조회하고자 하는 멤버의 ID 값")
+              ),
+              responseFields(
+                  pageHelper(getAwarderMeritLogResponse())
+              )));
     }
 
     @Test
