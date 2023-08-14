fef7fbd9ad84bdce93628d89fd9c6b026730de64
이지민 <min102602@naver.com>
2023-08-14 14:26:34 +0900

    feat : 선거 삭제 api 구현

--- src/main/java/com/keeper/homepage/domain/election/application/AdminElectionService.java
+++ src/main/java/com/keeper/homepage/domain/election/application/AdminElectionService.java
@@ -1,10 +1,13 @@
 package com.keeper.homepage.domain.election.application;
 
 
+import com.keeper.homepage.domain.election.application.convenience.ElectionDeleteService;
+import com.keeper.homepage.domain.election.application.convenience.ValidElectionFindService;
 import com.keeper.homepage.domain.election.dao.ElectionRepository;
 import com.keeper.homepage.domain.election.entity.Election;
 import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.global.error.BusinessException;
+import com.keeper.homepage.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +19,10 @@ public class AdminElectionService {
 
   private final ElectionRepository electionRepository;
 
+  private final ValidElectionFindService validElectionFindService;
+
+  private final ElectionDeleteService electionDeleteService;
+
   @Transactional
   public void createElection(Member member, String name, String description, Boolean isAvailable) {
     Election election = Election.builder()
@@ -26,6 +33,16 @@ public class AdminElectionService {
         .build();
 
     electionRepository.save(election);
+  }
+
+  @Transactional
+  public void deleteElection(long electionId) {
+    Election election = validElectionFindService.findById(electionId);
+
+    if (election.getIsAvailable() == true) {
+      throw new BusinessException(electionId, "electionId", ErrorCode.ELECTION_CANNOT_DELETE);
+    }
+    electionDeleteService.delete(election);
   }
 
 }
--- src/main/java/com/keeper/homepage/domain/election/dao/ElectionRepository.java
+++ src/main/java/com/keeper/homepage/domain/election/dao/ElectionRepository.java
@@ -1,9 +1,10 @@
 package com.keeper.homepage.domain.election.dao;
 
 import com.keeper.homepage.domain.election.entity.Election;
-import org.springframework.data.domain.Page;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ElectionRepository extends JpaRepository<Election, Long> {
+  Optional<Election> findByIdAndIdNot(Long electionId, Long virtualId);
 
 }
