017c0150aef0474b1b8420b2bc569d57a22c7cde
shkisme <shkisme0130@gmail.com>
2023-08-14 11:02:56 +0900

    fix: mockMvc test url 주소 수정

--- src/test/java/com/keeper/homepage/domain/election/api/AdminElectionApiTestHelper.java
+++ src/test/java/com/keeper/homepage/domain/election/api/AdminElectionApiTestHelper.java
@@ -1,9 +1,6 @@
 package com.keeper.homepage.domain.election.api;
 
 import static com.keeper.homepage.global.config.security.data.JwtType.ACCESS_TOKEN;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 
 import com.keeper.homepage.IntegrationTest;
@@ -15,7 +12,7 @@ import org.springframework.test.web.servlet.ResultActions;
 public class AdminElectionApiTestHelper extends IntegrationTest {
 
   ResultActions createElectionApi(String adminToken, ElectionCreateRequest request) throws Exception {
-    return mockMvc.perform(post("/elections")
+    return mockMvc.perform(post("/admin/elections")
         .content(asJsonString(request))
         .cookie(new Cookie(ACCESS_TOKEN.getTokenName(), adminToken))
         .contentType(MediaType.APPLICATION_JSON));
