75266c8fc98b2f57f5fd322af07f8a2bb3c4da7a
02ggang9 <02ggang9@gmail.com>
2023-08-15 17:07:22 +0900

    리뷰 내용 반영

--- src/main/java/com/keeper/homepage/domain/point/api/PointController.java
+++ src/main/java/com/keeper/homepage/domain/point/api/PointController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/points")
