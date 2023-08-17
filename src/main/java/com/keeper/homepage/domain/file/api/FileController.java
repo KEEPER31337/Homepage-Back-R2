33505510f63dd6f296eca91cd09f3c46165b3a35
shkisme <shkisme0130@gmail.com>
2023-08-17 18:51:09 +0900

    feat: 파일 다운로드 api 구현

---
+++ src/main/java/com/keeper/homepage/domain/file/api/FileController.java
@@ -0,0 +1,37 @@
+package com.keeper.homepage.domain.file.api;
+
+import com.keeper.homepage.domain.file.application.FileService;
+import com.keeper.homepage.domain.file.entity.FileEntity;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/files")
+public class FileController {
+
+  private final FileService fileService;
+
+  @GetMapping("/{fileId}")
+  public ResponseEntity<Resource> downloadFile(
+      @PathVariable long fileId
+  ) throws IOException {
+    FileEntity file = fileService.getFile(fileId);
+    Resource resource = fileService.downloadFile(file);
+    String fileName = fileService.getFileName(file);
+    return ResponseEntity.ok()
+        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+        .body(resource);
+  }
+}
---
+++ src/main/java/com/keeper/homepage/domain/file/application/FileService.java
@@ -0,0 +1,40 @@
+package com.keeper.homepage.domain.file.application;
+
+import static com.keeper.homepage.global.error.ErrorCode.FILE_NOT_FOUND;
+
+import com.keeper.homepage.domain.file.dao.FileRepository;
+import com.keeper.homepage.domain.file.entity.FileEntity;
+import com.keeper.homepage.global.error.BusinessException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.UriUtils;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FileService {
+
+  private final FileRepository fileRepository;
+
+  public FileEntity getFile(long fileId) {
+    return fileRepository.findById(fileId)
+        .orElseThrow(() -> new BusinessException(fileId, "fileId", FILE_NOT_FOUND));
+  }
+
+  public Resource downloadFile(FileEntity file) throws IOException {
+    Path path = Paths.get(file.getFilePath());
+    return new InputStreamResource(Files.newInputStream(path));
+  }
+
+  public String getFileName(FileEntity file) {
+    return UriUtils.encode(file.getFileName(), StandardCharsets.UTF_8);
+  }
+}
