b545b220c8f260ab1f1e5e6ef80d337ee0f13e02
02ggang9 <02ggang9@gmail.com>
2023-08-21 22:24:26 +0900

    feat: 선거 후보자 등록 Service 코드 작성

--- src/main/java/com/keeper/homepage/domain/election/application/AdminElectionService.java
+++ src/main/java/com/keeper/homepage/domain/election/application/AdminElectionService.java
@@ -1,14 +1,23 @@
 package com.keeper.homepage.domain.election.application;
 
 
+import static com.keeper.homepage.global.error.ErrorCode.MEMBER_JOB_NOT_FOUND;
+
 import com.keeper.homepage.domain.election.application.convenience.ElectionDeleteService;
 import com.keeper.homepage.domain.election.application.convenience.ValidElectionFindService;
+import com.keeper.homepage.domain.election.dao.ElectionCandidateRepository;
 import com.keeper.homepage.domain.election.dao.ElectionRepository;
 import com.keeper.homepage.domain.election.dto.response.ElectionResponse;
 import com.keeper.homepage.domain.election.entity.Election;
+import com.keeper.homepage.domain.election.entity.ElectionCandidate;
+import com.keeper.homepage.domain.member.application.convenience.MemberFindService;
+import com.keeper.homepage.domain.member.dao.role.MemberJobRepository;
 import com.keeper.homepage.domain.member.entity.Member;
+import com.keeper.homepage.domain.member.entity.job.MemberJob;
+import com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType;
 import com.keeper.homepage.global.error.BusinessException;
 import com.keeper.homepage.global.error.ErrorCode;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,10 +30,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class AdminElectionService {
 
   private final ElectionRepository electionRepository;
-
   private final ValidElectionFindService validElectionFindService;
-
   private final ElectionDeleteService electionDeleteService;
+  private final ElectionCandidateRepository electionCandidateRepository;
+  private final MemberJobRepository memberJobRepository;
+  private final MemberFindService memberFindService;
+
 
   @Transactional
   public void createElection(Member member, String name, String description, Boolean isAvailable) {
@@ -59,6 +70,22 @@ public class AdminElectionService {
   public Page<ElectionResponse> getElections(Pageable pageable) {
     return validElectionFindService.findAll(pageable)
         .map(ElectionResponse::from);
+  }
+
+  @Transactional
+  public void registerCandidate(long electionId, long candidateId, String description, long memberJobId) {
+    Election election = validElectionFindService.findById(electionId);
+    Member candidate = memberFindService.findById(candidateId);
+    MemberJob memberJob = memberJobRepository.findById(memberJobId)
+        .orElseThrow(() -> new BusinessException(memberJobId, "memberJobId", MEMBER_JOB_NOT_FOUND));
+
+    electionCandidateRepository.save(ElectionCandidate.builder()
+        .election(election)
+        .member(candidate)
+        .memberJob(memberJob)
+        .description(description)
+        .voteCount(0L)
+        .build());
   }
 
 }
