6eeed629c7e1bde51ff8db52b4e44f6c64016331
이지민 <min102602@naver.com>
2023-08-22 19:13:33 +0900

    fix : `null` 체크 방식 변경 - 이렇게 하면 check 되겠죠..?

--- src/main/java/com/keeper/homepage/domain/election/dto/request/ElectionCandidatesRegisterRequest.java
+++ src/main/java/com/keeper/homepage/domain/election/dto/request/ElectionCandidatesRegisterRequest.java
@@ -2,6 +2,7 @@ package com.keeper.homepage.domain.election.dto.request;
 
 import static lombok.AccessLevel.PRIVATE;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -15,8 +16,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = PRIVATE)
 public class ElectionCandidatesRegisterRequest {
 
-  @NotNull(message = "후보자 아이디를 입력해주세요.")
-  private List<Long> candidateIds;
+  @NotEmpty(message = "후보자 아이디를 입력해주세요.")
+  private List<@NotNull Long> candidateIds;
 
   @NotNull(message = "설명을 입력해주세요.")
   private String description;
