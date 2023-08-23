23c744395aa8398e80fc088a7f3a40413b3a4aff
이지민 <min102602@naver.com>
2023-08-23 20:45:46 +0900

    fix : memberJobId 검증 방식 변경

--- src/main/java/com/keeper/homepage/domain/election/application/convenience/ValidCandidateService.java
+++ src/main/java/com/keeper/homepage/domain/election/application/convenience/ValidCandidateService.java
@@ -1,5 +1,8 @@
 package com.keeper.homepage.domain.election.application.convenience;
 
+import static com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType.ROLE_부회장;
+import static com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType.ROLE_총무;
+import static com.keeper.homepage.domain.member.entity.job.MemberJob.MemberJobType.ROLE_회장;
 import static com.keeper.homepage.global.error.ErrorCode.ELECTION_CANDIDATE_CANNOT_REGISTER;
 import static com.keeper.homepage.global.error.ErrorCode.ELECTION_CANDIDATE_NOT_FOUND;
 import static com.keeper.homepage.global.error.ErrorCode.MEMBER_JOB_NOT_FOUND;
@@ -9,7 +12,8 @@ import com.keeper.homepage.domain.election.entity.ElectionCandidate;
 import com.keeper.homepage.domain.member.dao.role.MemberJobRepository;
 import com.keeper.homepage.domain.member.entity.job.MemberJob;
 import com.keeper.homepage.global.error.BusinessException;
-import java.util.EnumSet;
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +24,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class ValidCandidateService {
 
   private static final long VIRTUAL_ELECTION_CANDIDATE_ID = 1;
+  private static final List<Long> VALID_CANDIDATE_JOB_IDS = Arrays.asList(ROLE_회장.getId(), ROLE_부회장.getId(),
+      ROLE_총무.getId());
 
   private final ElectionCandidateRepository electionCandidateRepository;
   private final MemberJobRepository memberJobRepository;
@@ -30,29 +36,15 @@ public class ValidCandidateService {
   }
 
   public MemberJob findJobById(long memberJobId) {
-    if (!isValidMemberJobId(memberJobId)) {
+    if (!isValidCandidateJobId(memberJobId)) {
       throw new BusinessException(memberJobId, "memJobId", ELECTION_CANDIDATE_CANNOT_REGISTER);
     }
     return memberJobRepository.findById(memberJobId)
         .orElseThrow(() -> new BusinessException(memberJobId, "memberJobId", MEMBER_JOB_NOT_FOUND));
   }
 
-  private boolean isValidMemberJobId(long memberJobId) {
-    return EnumSet.of(MemberJobId.ROLE_회장, MemberJobId.ROLE_부회장, MemberJobId.ROLE_총무).stream()
-        .anyMatch(jobId -> jobId.id == memberJobId);
-  }
-
-  public enum MemberJobId {
-    ROLE_회장(1),
-    ROLE_부회장(2),
-    ROLE_총무(8);
-
-    private final long id;
-
-    MemberJobId(long id) {
-      this.id = id;
-    }
-
+  private boolean isValidCandidateJobId(long memberJobId) {
+    return VALID_CANDIDATE_JOB_IDS.contains(memberJobId);
   }
 
 }
