33e86070170dd23748827d4e3ac44b45762e5307
lcqff <yeonnu82g@gmail.com>
2023-08-24 14:19:53 +0900

    feat: 입력값 유효성 검사

--- src/main/java/com/keeper/homepage/domain/member/dto/request/ProfileUpdateRequest.java
+++ src/main/java/com/keeper/homepage/domain/member/dto/request/ProfileUpdateRequest.java
@@ -1,7 +1,11 @@
 package com.keeper.homepage.domain.member.dto.request;
 
+import static com.keeper.homepage.domain.member.entity.embedded.RealName.REAL_NAME_REGEX;
+import static com.keeper.homepage.domain.member.entity.embedded.StudentId.STUDENT_ID_INVALID;
+import static com.keeper.homepage.domain.member.entity.embedded.StudentId.STUDENT_ID_REGEX;
 import static lombok.AccessLevel.PRIVATE;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.keeper.homepage.domain.member.entity.Member;
 import com.keeper.homepage.domain.member.entity.embedded.Profile;
 import com.keeper.homepage.domain.member.entity.embedded.RealName;
@@ -9,6 +13,7 @@ import com.keeper.homepage.domain.member.entity.embedded.StudentId;
 import com.keeper.homepage.domain.post.entity.Post;
 import io.micrometer.common.lang.Nullable;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,20 +26,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = PRIVATE)
 public class ProfileUpdateRequest {
 
-  @NotNull
-  private RealName realName;
-
-  @NotNull
-  private StudentId studentId;
-
-  @Nullable
+  @Pattern(regexp = REAL_NAME_REGEX, message = RealName.REAL_NAME_INVALID)
+  private String realName;
+  @JsonFormat(pattern = "yyyy.MM.dd")
   private LocalDate birthday;
+  @Pattern(regexp = STUDENT_ID_REGEX, message = STUDENT_ID_INVALID)
+  private String studentId;
 
   public Profile toEntity() {
     return Profile.builder()
-        .realName(realName)
-        .studentId(studentId)
-        .birthday(birthday)
+        .realName(RealName.from(this.realName))
+        .studentId(StudentId.from(this.studentId))
+        .birthday(this.birthday)
         .build();
   }
 
