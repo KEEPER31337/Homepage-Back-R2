ecb03ebf40d5474b726300319f4906040b81eb95
shkisme <shkisme0130@gmail.com>
2023-09-09 15:17:32 +0900

    fix: 본인이 작성한 게시글의 첨부파일 조회 시 포인트 차감되지 않도록 수정

--- src/main/java/com/keeper/homepage/domain/post/application/PostService.java
+++ src/main/java/com/keeper/homepage/domain/post/application/PostService.java
@@ -198,7 +198,7 @@ public class PostService {
   public List<FileResponse> getFiles(Member member, long postId) {
     Post post = validPostFindService.findById(postId);
 
-    if (post.isCategory(시험게시판) && !member.isRead(post)) {
+    if (post.isCategory(시험게시판) && !member.isRead(post) && !post.isMine(member)) {
       member.read(post);
       member.minusPoint(EXAM_READ_DEDUCTION_POINT, EXAM_READ_POINT_MESSAGE);
     }
