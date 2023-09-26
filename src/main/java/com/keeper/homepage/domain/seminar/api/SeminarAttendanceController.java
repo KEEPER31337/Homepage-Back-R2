331b95ff477dab90fbad852b70f26ea8d8c62521
shkisme <shkisme0130@gmail.com>
2023-09-26 15:31:39 +0900

    feat: @Future 어노테이션 에러 메시지 추가

--- src/main/java/com/keeper/homepage/domain/seminar/api/SeminarAttendanceController.java
+++ src/main/java/com/keeper/homepage/domain/seminar/api/SeminarAttendanceController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/seminars")
@@ -35,7 +37,8 @@ public class SeminarAttendanceController {
       @PathVariable Long seminarId,
       @LoginMember Member member,
       @RequestBody @Valid SeminarAttendanceCodeRequest request) {
-    SeminarAttendanceResponse response = seminarAttendanceService.attendance(seminarId, member, request.attendanceCode());
+    SeminarAttendanceResponse response = seminarAttendanceService.attendance(seminarId, member,
+        request.attendanceCode());
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 
